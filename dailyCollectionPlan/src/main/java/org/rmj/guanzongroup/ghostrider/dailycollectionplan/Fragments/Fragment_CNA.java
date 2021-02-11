09d506a12ad9be7ad1140ec369c5139c6ab32239
kpdumaguin <dkentpatrickd@gmail.com>
2021-02-11 14:07:53 +0800

    KENT - (DCP) Fragment_CNA layout

---
+++ dailyCollectionPlan/src/main/java/org/rmj/guanzongroup/ghostrider/dailycollectionplan/Fragments/Fragment_CNA.java
@@ -0,0 +1,28 @@
+package org.rmj.guanzongroup.ghostrider.dailycollectionplan.Fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.rmj.guanzongroup.ghostrider.dailycollectionplan.R;
+
+public class Fragment_CNA extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_cna, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
