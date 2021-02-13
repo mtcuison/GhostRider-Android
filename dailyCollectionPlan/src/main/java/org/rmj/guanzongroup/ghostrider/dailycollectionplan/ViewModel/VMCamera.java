a1d5a88ab7e15bda29590a05e928eec696201481
unclejo <jonsabiniano03@gmail.com>
2021-02-13 09:18:45 +0800

    Show camera in a fragment

---
+++ dailyCollectionPlan/src/main/java/org/rmj/guanzongroup/ghostrider/dailycollectionplan/ViewModel/VMCamera.java
@@ -0,0 +1,7 @@
+package org.rmj.guanzongroup.ghostrider.dailycollectionplan.ViewModel;
+
+import androidx.lifecycle.ViewModel;
+
+public class VMCamera extends ViewModel {
+    // TODO: Implement the ViewModel
+}
