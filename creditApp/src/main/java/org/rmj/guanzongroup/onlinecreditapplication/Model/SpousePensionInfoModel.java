62abdaa53f3d1326076a84f2fe835be6278e3537
kpdumaguin <dkentpatrickd@gmail.com>
2021-02-23 11:27:41 +0800

    2/23/21 - Integsys - Created SpousePensionInfoModel.java along with corresponding unit test.

--- creditApp/src/main/java/org/rmj/guanzongroup/onlinecreditapplication/Model/SpousePensionInfoModel.java
+++ creditApp/src/main/java/org/rmj/guanzongroup/onlinecreditapplication/Model/SpousePensionInfoModel.java
@@ -1,5 +1,99 @@
 package org.rmj.guanzongroup.onlinecreditapplication.Model;
 
-public class SpousePensionInfoModel {
+import org.rmj.g3appdriver.GRider.Etc.InputChecker;
+
+public class SpousePensionInfoModel extends InputChecker {
     // TODO: Test first
+    private String sMgs;
+    private String sPensionSector;
+    private String sPensionAmt;
+    private String sRetirementYr;
+    private String sOtherSrc;
+    private String sOtherSrcIncx;
+
+    public String getsMgs() {
+        return sMgs;
+    }
+
+    public String getsPensionSector() {
+        return sPensionSector;
+    }
+
+    public void setsPensionSector(String sPensionSector) {
+        this.sPensionSector = sPensionSector;
+    }
+
+    public String getsPensionAmt() {
+        return sPensionAmt;
+    }
+
+    public void setsPensionAmt(String sPensionAmt) {
+        this.sPensionAmt = sPensionAmt;
+    }
+
+    public String getsRetirementYr() {
+        return sRetirementYr;
+    }
+
+    public void setsRetirementYr(String sRetirementYr) {
+        this.sRetirementYr = sRetirementYr;
+    }
+
+    public String getsOtherSrc() {
+        return sOtherSrc;
+    }
+
+    public void setsOtherSrc(String sOtherSrc) {
+        this.sOtherSrc = sOtherSrc;
+    }
+
+    public String getsOtherSrcIncx() {
+        return sOtherSrcIncx;
+    }
+
+    public void setsOtherSrcIncx(String sOtherSrcIncx) {
+        this.sOtherSrcIncx = sOtherSrcIncx;
+    }
+
+    public boolean isPensionDataValid(){
+        return isPensionSectorValid() &&
+                isPensionAmtValid() &&
+                isRetirementYrValid() &&
+                isOtherSrcIncxValid();
+    }
+
+    private boolean isPensionSectorValid() {
+        if(sPensionSector.isEmpty()) {
+            sMgs = "Please select pension sector.";
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isPensionAmtValid() {
+        if(isStringAllZero(sPensionAmt) || sPensionAmt.trim().isEmpty()) {
+            sMgs = "Please enter an estimated amount of pension.";
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isRetirementYrValid() {
+        if(sRetirementYr.trim().isEmpty()) {
+            sMgs = "Please enter retirement year.";
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isOtherSrcIncxValid() {
+        if(!sOtherSrc.trim().isEmpty()) {
+            if(isStringAllZero(sOtherSrcIncx) || sOtherSrcIncx.trim().isEmpty()) {
+                sMgs = "Please enter an estimated amount in source of income.";
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
--- creditApp/src/test/java/org/rmj/guanzongroup/onlinecreditapplication/Model/SpousePensionInfoModelTest.java
+++ creditApp/src/test/java/org/rmj/guanzongroup/onlinecreditapplication/Model/SpousePensionInfoModelTest.java
@@ -2,6 +2,57 @@ package org.rmj.guanzongroup.onlinecreditapplication.Model;
 
 import junit.framework.TestCase;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class SpousePensionInfoModelTest extends TestCase {
-    SpousePensionInfoModel testObj = new SpousePensionInfoModel();
+
+    private SpousePensionInfoModel testObj;
+    private static final String EMPTY_STRING= "";
+    private static final String STRING_ZERO = "0";
+    private static final String STRING_ONE = "1";
+    private static final String FAKE_AMOUNT = "12000";
+    private static final String FAKE_YEAR = "1900";
+    private static final String FAKE_INCOME_SOURCE = "Business";
+
+    @Before
+    public void setUp() {
+        testObj = new SpousePensionInfoModel();
+        testObj.setsPensionSector(STRING_ZERO);
+        testObj.setsPensionAmt(FAKE_AMOUNT);
+        testObj.setsRetirementYr(FAKE_YEAR);
+        testObj.setsOtherSrc(FAKE_INCOME_SOURCE);
+        testObj.setsOtherSrcIncx(FAKE_AMOUNT);
+    }
+
+    @Test
+    public void test_getPensionSector() {
+        assertEquals(STRING_ZERO, testObj.getsPensionSector());
+    }
+
+    @Test
+    public void test_getPensionAmt() {
+        assertEquals(FAKE_AMOUNT, testObj.getsPensionAmt());
+    }
+
+    @Test
+    public void test_getRetirementYr() {
+        assertEquals(FAKE_YEAR, testObj.getsRetirementYr());
+    }
+
+    @Test
+    public void test_getOtherSrc() {
+        assertEquals(FAKE_INCOME_SOURCE, testObj.getsOtherSrc());
+    }
+
+    @Test
+    public void test_getOtherSrcIncx() {
+        assertEquals(FAKE_AMOUNT,testObj.getsOtherSrcIncx());
+    }
+
+    @Test
+    public void test_isPensionDataValid() {
+        assertTrue(testObj.isPensionDataValid());
+    }
+
 }
