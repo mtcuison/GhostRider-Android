0b23bfe5430d2bcc7d3d0c8fb26a8779e8a10b43
kpdumaguin <dkentpatrickd@gmail.com>
2021-02-23 13:45:08 +0800

    2/23/21 - Integsys - Created VMSpousePensionInfo.java.

--- creditApp/src/main/java/org/rmj/guanzongroup/onlinecreditapplication/ViewModel/VMSpousePensionInfo.java
+++ creditApp/src/main/java/org/rmj/guanzongroup/onlinecreditapplication/ViewModel/VMSpousePensionInfo.java
@@ -1,7 +1,89 @@
 package org.rmj.guanzongroup.onlinecreditapplication.ViewModel;
 
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class VMSpousePensionInfo extends ViewModel {
-    // TODO: Implement the ViewModel
+import org.rmj.g3appdriver.GRider.Database.Entities.ECreditApplicantInfo;
+import org.rmj.g3appdriver.GRider.Database.Repositories.RCreditApplicant;
+import org.rmj.gocas.base.GOCASApplication;
+import org.rmj.guanzongroup.onlinecreditapplication.Model.SpousePensionInfoModel;
+import org.rmj.guanzongroup.onlinecreditapplication.Model.ViewModelCallBack;
+
+import java.util.Objects;
+
+public class VMSpousePensionInfo extends AndroidViewModel {
+    private static final String TAG = VMSpousePensionInfo.class.getSimpleName();
+    private final GOCASApplication poGoCas;
+    private final RCreditApplicant poCreditApp;
+
+    private final MutableLiveData<String> psTransNo = new MutableLiveData<>();
+    private final MutableLiveData<String> psPensionSec = new MutableLiveData<>();
+
+    public VMSpousePensionInfo(@NonNull Application application) {
+        super(application);
+        this.poGoCas = new GOCASApplication();
+        this.poCreditApp = new RCreditApplicant(application);
+    }
+
+    public void setTransNox(String transNox) {
+        this.psTransNo.setValue(transNox);
+    }
+
+    public void setPensionSec(String fsPensionSec) {
+        this.psPensionSec.setValue(fsPensionSec);
+    }
+
+    public LiveData<ECreditApplicantInfo> getActiveGOCasApplication(){
+        return poCreditApp.getCreditApplicantInfoLiveData(psTransNo.getValue());
+    }
+
+    public boolean setDetailInfo(String fsDetailInfo){
+        try{
+            poGoCas.setData(fsDetailInfo);
+            return true;
+        } catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean Save(SpousePensionInfoModel infoModel, ViewModelCallBack callBack) {
+        try{
+            infoModel.setsPensionSector(psPensionSec.getValue());
+            if(infoModel.isPensionDataValid()) {
+                poGoCas.SpouseMeansInfo().PensionerInfo().setSource(infoModel.getsPensionSector());
+                poGoCas.SpouseMeansInfo().PensionerInfo().setAmount(infoModel.getsPensionAmt());
+                poGoCas.SpouseMeansInfo().PensionerInfo().setYearRetired(infoModel.getsRetirementYr());
+                poGoCas.SpouseMeansInfo().setOtherIncomeNature(infoModel.getsOtherSrc());
+                poGoCas.SpouseMeansInfo().setOtherIncomeAmount(infoModel.getsOtherSrcIncx());
+
+                ECreditApplicantInfo info = new ECreditApplicantInfo();
+                info.setTransNox(Objects.requireNonNull(psTransNo.getValue()));
+                info.setDetlInfo(poGoCas.toJSONString());
+                info.setClientNm(poGoCas.ApplicantInfo().getClientName());
+                poCreditApp.updateGOCasData(info);
+
+                Log.e(TAG, poGoCas.SpouseMeansInfo().PensionerInfo().toJSONString());
+                Log.e(TAG, "GOCAS Full JSON String : " + poGoCas.toJSONString());
+                callBack.onSaveSuccessResult("Success");
+                return true;
+            }
+            else {
+                callBack.onFailedResult(infoModel.getsMgs());
+                return false;
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            callBack.onFailedResult(e.getMessage());
+            return false;
+        }
+    }
+
 }
