bd03222e0c146e1244297900648955c8b8b6b519
mikegarcia <mikegarcia8748@gmail.com>
2021-02-25 16:42:50 +0800

    Recreated UI for Raffle entry. From Fragment to Activity_RaffleEntry.class

--- promotions/src/main/java/org/rmj/guanzongroup/promotions/Model/RaffleEntry.java
+++ promotions/src/main/java/org/rmj/guanzongroup/promotions/Model/RaffleEntry.java
@@ -3,67 +3,139 @@ package org.rmj.guanzongroup.promotions.Model;
 public class RaffleEntry {
     private String BranchCodexx;
     private String CustomerName;
+    private String CustomerAddx;
+    private String CustomerTown;
+    private String CustomerProv;
     private String DocumentType;
     private String DocumentNoxx;
     private String MobileNumber;
 
-    public RaffleEntry(String branchCodexx, String customerName, String documentNoxx, String mobileNumber) {
-        BranchCodexx = branchCodexx;
-        CustomerName = customerName;
-        DocumentNoxx = documentNoxx;
-        MobileNumber = mobileNumber;
+    private String message;
+
+    public RaffleEntry() {
     }
 
-    public void setDocumentType(String documentType) {
-        DocumentType = documentType;
+    public String getMessage() {
+        return message;
     }
 
     public String getBranchCodexx() {
         return BranchCodexx;
     }
 
+    public void setBranchCodexx(String branchCodexx) {
+        BranchCodexx = branchCodexx;
+    }
+
     public String getCustomerName() {
         return CustomerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        CustomerName = customerName;
+    }
+
+    public String getCustomerAddx() {
+        return CustomerAddx;
+    }
+
+    public void setCustomerAddx(String customerAddx) {
+        CustomerAddx = customerAddx;
+    }
+
+    public String getCustomerProv() {
+        return CustomerProv;
+    }
+
+    public void setCustomerProv(String customerProv) {
+        CustomerProv = customerProv;
+    }
+
+    public String getCustomerTown() {
+        return CustomerTown;
+    }
+
+    public void setCustomerTown(String customerTown) {
+        CustomerTown = customerTown;
     }
 
     public String getDocumentType() {
         return DocumentType;
     }
 
+    public void setDocumentType(String documentType) {
+        DocumentType = documentType;
+    }
+
     public String getDocumentNoxx() {
         return DocumentNoxx;
+    }
+
+    public void setDocumentNoxx(String documentNoxx) {
+        DocumentNoxx = documentNoxx;
     }
 
     public String getMobileNumber() {
         return MobileNumber;
     }
 
+    public void setMobileNumber(String mobileNumber) {
+        MobileNumber = mobileNumber;
+    }
+
     public boolean isValidInfo(){
         return isNameValid() &&
+                isTownValid() &&
                 isDocumentNoValid() &&
                 isMobileNoValid() &&
                 isDocumentTypeValid();
     }
 
-    private boolean isNameValid(){
-        return !CustomerName.trim().isEmpty();
+    public boolean isNameValid(){
+        if(CustomerName.trim().isEmpty()){
+            message = "Please enter customer name";
+            return false;
+        }
+        return true;
     }
 
-    private boolean isDocumentNoValid(){
-        return !DocumentNoxx.trim().isEmpty();
+    public boolean isTownValid(){
+        if(CustomerTown.trim().isEmpty()){
+            message = "Please enter town";
+            return false;
+        }
+        return true;
     }
 
-    private boolean isMobileNoValid(){
+    public boolean isDocumentNoValid(){
+        if(DocumentNoxx.trim().isEmpty()){
+            message = "Please enter document number";
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isMobileNoValid(){
         if(MobileNumber.trim().isEmpty()){
+            message = "Please enter mobile number";
             return false;
         }
         if(MobileNumber.length()!=11){
+            message = "Please enter valid mobile number";
             return false;
         }
-        return MobileNumber.substring(0, 2).equalsIgnoreCase("09");
+        if(!MobileNumber.substring(0, 2).equalsIgnoreCase("09")){
+            message = "Mobile number must start with '09'";
+            return false;
+        }
+        return true;
     }
 
-    private boolean isDocumentTypeValid(){
-        return !DocumentType.trim().isEmpty();
+    public boolean isDocumentTypeValid(){
+        if(DocumentType.trim().isEmpty()){
+            message = "Please select document type";
+            return false;
+        }
+        return true;
     }
 }
