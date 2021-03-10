3f55cb7da91575acb69bd0a28096e3a01f22bfef
mikegarcia <mikegarcia8748@gmail.com>
2021-03-10 16:27:15 +0800

    Added Check Payment for pay transaction Added import bank list

---
+++ dailyCollectionPlan/src/main/java/org/rmj/guanzongroup/ghostrider/dailycollectionplan/Dialog/DialogCheckPayment.java
@@ -0,0 +1,86 @@
+package org.rmj.guanzongroup.ghostrider.dailycollectionplan.Dialog;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.rmj.g3appdriver.GRider.Etc.GToast;
+import org.rmj.guanzongroup.ghostrider.dailycollectionplan.Etc.OnBirthSetListener;
+import org.rmj.guanzongroup.ghostrider.dailycollectionplan.R;
+
+import java.util.Objects;
+
+public class DialogCheckPayment {
+    private static final String TAG = DialogCheckPayment.class.getSimpleName();
+
+    private AlertDialog poDialogx;
+    private final Context context;
+
+    private String lsBank;
+
+    public interface OnCheckPaymentDialogListener{
+        void OnConfirm(AlertDialog dialog, String bank, String date, String checkNo, String AcctNo);
+        void OnCancel(AlertDialog dialog);
+    }
+
+    public DialogCheckPayment(Context context) {
+        this.context = context;
+    }
+
+    public void initDialog(String[] bankList, OnCheckPaymentDialogListener listener){
+        View view = LayoutInflater.from(context).inflate(R.layout.dialog_check_payment, null, false);
+        AlertDialog.Builder loBuilder = new AlertDialog.Builder(context);
+        loBuilder.setView(view)
+                .setCancelable(false);
+        poDialogx = loBuilder.create();
+        poDialogx.setCancelable(false);
+
+        AutoCompleteTextView txtBankNme = view.findViewById(R.id.txt_dcpBankName);
+        TextInputEditText txtCheckDt = view.findViewById(R.id.txt_dcpCheckDate);
+        TextInputEditText txtCheckNo = view.findViewById(R.id.txt_dcpCheckNo);
+        TextInputEditText txtAccntNo = view.findViewById(R.id.txt_dcpAcctNumber);
+
+        Button btnConfirm = view.findViewById(R.id.btn_dcpConfirm);
+        Button btnCancel = view.findViewById(R.id.btn_cancel);
+
+        ArrayAdapter<String> loAdapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_dropdown_item, bankList);
+        txtBankNme.setAdapter(loAdapter);
+
+        txtBankNme.setOnItemClickListener((parent, view1, position, id) -> lsBank = txtBankNme.getText().toString());
+
+        txtCheckDt.addTextChangedListener(new OnBirthSetListener(txtCheckDt));
+
+        btnConfirm.setOnClickListener(v -> {
+            String lsCheckDt = Objects.requireNonNull(txtCheckDt.getText()).toString();
+            String lsCheckNo = Objects.requireNonNull(txtCheckNo.getText()).toString();
+            String lsAccntNo = Objects.requireNonNull(txtAccntNo.getText()).toString();
+            if(lsBank.trim().isEmpty()){
+                GToast.CreateMessage(context, "Please enter bank name", GToast.ERROR).show();
+            } else if(lsCheckDt.trim().isEmpty()){
+                GToast.CreateMessage(context, "Please enter check date", GToast.ERROR).show();
+            } else if(lsCheckNo.trim().isEmpty()){
+                GToast.CreateMessage(context, "Please enter check no.", GToast.ERROR).show();
+            } else if(lsAccntNo.trim().isEmpty()){
+                GToast.CreateMessage(context, "Please enter check account no.", GToast.ERROR).show();
+            } else {
+                listener.OnConfirm(poDialogx, lsBank, lsCheckDt, lsCheckNo, lsAccntNo);
+            }
+        });
+
+        btnCancel.setOnClickListener(v -> listener.OnCancel(poDialogx));
+    }
+
+    public void show(){
+        poDialogx.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        poDialogx.getWindow().getAttributes().windowAnimations = org.rmj.g3appdriver.R.style.PopupAnimation;
+        poDialogx.show();
+    }
+}
---
+++ g3appdriver/src/main/java/org/rmj/g3appdriver/GRider/ImportData/Import_BankList.java
@@ -0,0 +1,112 @@
+package org.rmj.g3appdriver.GRider.ImportData;
+
+import android.app.Application;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.rmj.g3appdriver.GRider.Constants.AppConstants;
+import org.rmj.g3appdriver.GRider.Database.Repositories.RBankInfo;
+import org.rmj.g3appdriver.GRider.Http.HttpHeaders;
+import org.rmj.g3appdriver.utils.ConnectionUtil;
+import org.rmj.g3appdriver.utils.WebClient;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+import static org.rmj.g3appdriver.utils.WebApi.URL_DOWNLOAD_BANK_INFO;
+
+public class Import_BankList implements ImportInstance{
+    private static final String TAG = Import_BankList.class.getSimpleName();
+
+    private Application instance;
+
+    public Import_BankList(Application instance) {
+        this.instance = instance;
+    }
+
+    @Override
+    public void ImportData(ImportDataCallback callback) {
+        try{
+            JSONObject loJson = new JSONObject();
+            loJson.put("bsearch", true);
+            loJson.put("descript", "All");
+            new ImportBankListTask(instance, callback).execute(loJson);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private static class ImportBankListTask extends AsyncTask<JSONObject, Void, String>{
+        private final ImportDataCallback callback;
+        private final HttpHeaders headers;
+        private final ConnectionUtil conn;
+        private final RBankInfo poBank;
+
+        public ImportBankListTask(Application instance, ImportDataCallback callback){
+            this.headers = HttpHeaders.getInstance(instance);
+            this.conn = new ConnectionUtil(instance);
+            this.poBank = new RBankInfo(instance);
+            this.callback = callback;
+        }
+
+
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        @Override
+        protected String doInBackground(JSONObject... jsonObjects) {
+            String response = "";
+            try {
+                if(conn.isDeviceConnected()) {
+                    response = WebClient.httpsPostJSon(URL_DOWNLOAD_BANK_INFO, jsonObjects[0].toString(), headers.getHeaders());
+                    JSONObject loJson = new JSONObject(Objects.requireNonNull(response));
+                    Log.e(TAG, loJson.getString("result"));
+                    String lsResult = loJson.getString("result");
+                    if(lsResult.equalsIgnoreCase("success")){
+                        JSONArray laJson = loJson.getJSONArray("detail");
+                        if(!poBank.insertBankInfo(laJson)){
+                            response = AppConstants.ERROR_SAVING_TO_LOCAL();
+                        }
+                    } else {
+                        JSONObject loError = loJson.getJSONObject("error");
+                        String message = loError.getString("message");
+                        callback.OnFailedImportData(message);
+                    }
+                } else {
+                    response = AppConstants.NO_INTERNET();
+                }
+            } catch (Exception e) {
+                Log.e(TAG, Arrays.toString(e.getStackTrace()));
+                e.printStackTrace();
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                JSONObject loJson = new JSONObject(s);
+                Log.e(TAG, loJson.getString("result"));
+                String lsResult = loJson.getString("result");
+                if(lsResult.equalsIgnoreCase("success")){
+                    callback.OnSuccessImportData();
+                } else {
+                    JSONObject loError = loJson.getJSONObject("error");
+                    String message = loError.getString("message");
+                    callback.OnFailedImportData(message);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                callback.OnFailedImportData(e.getMessage());
+            } catch (Exception e) {
+                e.printStackTrace();
+                callback.OnFailedImportData(e.getMessage());
+            }
+        }
+    }
+}
