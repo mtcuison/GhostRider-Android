c3441edab2cc1e119fe0b5d7f3c88bbfac23e9cd
unclejo <jonsabiniano03@gmail.com>
2021-03-29 16:47:15 +0800

    Set griderScanner to dynamic module Update codes for importing credit online application list to local database from server Update error fetching data from local database after importing Add UpdateFileNameAndFolder.java for updating image name and folder name

---
+++ CreditEvaluator/src/main/java/org/rmj/guanzongroup/ghostrider/creditevaluator/Activity/Activity_CIApplication.java
@@ -0,0 +1,16 @@
+package org.rmj.guanzongroup.ghostrider.creditevaluator.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.rmj.guanzongroup.ghostrider.creditevaluator.R;
+
+public class Activity_CIApplication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ci_application);
+    }
+}
