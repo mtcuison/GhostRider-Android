feb9632c116d6244bd1241c95612ceccd43408db
mikegarcia8748 <mikegarcia8748@gmail.com>
2021-04-12 14:55:18 +0800

    Remove unused class in App module

---
+++ ahMonitoring/src/main/java/org/rmj/guanzongroup/ghostrider/ahmonitoring/Fragment/Fragment_BranchOpening.java
@@ -0,0 +1,39 @@
+package org.rmj.guanzongroup.ghostrider.ahmonitoring.Fragment;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.rmj.guanzongroup.ghostrider.ahmonitoring.R;
+import org.rmj.guanzongroup.ghostrider.ahmonitoring.ViewModel.VMBranchOpening;
+
+public class Fragment_BranchOpening extends Fragment {
+
+    private VMBranchOpening mViewModel;
+
+    public static Fragment_BranchOpening newInstance() {
+        return new Fragment_BranchOpening();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_branch_opening, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(VMBranchOpening.class);
+        // TODO: Use the ViewModel
+    }
+
+}
