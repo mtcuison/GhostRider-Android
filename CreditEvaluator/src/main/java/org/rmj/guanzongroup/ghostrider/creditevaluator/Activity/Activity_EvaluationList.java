dcb2df3de1996a74ff7f29f89f3965e3553bb488
kpdumaguin <dkentpatrickd@gmail.com>
2021-04-12 10:55:57 +0800

    CreditEvaluator - Add Application - Added dialog for adding application.

--- CreditEvaluator/src/main/java/org/rmj/guanzongroup/ghostrider/creditevaluator/Activity/Activity_EvaluationList.java
+++ CreditEvaluator/src/main/java/org/rmj/guanzongroup/ghostrider/creditevaluator/Activity/Activity_EvaluationList.java
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
@@ -24,6 +25,7 @@ import org.rmj.g3appdriver.GRider.Etc.GToast;
 import org.rmj.g3appdriver.GRider.Etc.LoadDialog;
 import org.rmj.g3appdriver.GRider.Etc.MessageBox;
 import org.rmj.guanzongroup.ghostrider.creditevaluator.Adapter.CreditEvaluationListAdapter;
+import org.rmj.guanzongroup.ghostrider.creditevaluator.Dialog.DialogAddApplication;
 import org.rmj.guanzongroup.ghostrider.creditevaluator.Model.CreditEvaluationModel;
 import org.rmj.guanzongroup.ghostrider.creditevaluator.R;
 import org.rmj.guanzongroup.ghostrider.creditevaluator.ViewModel.VMEvaluationList;
@@ -83,7 +85,25 @@ public class Activity_EvaluationList extends AppCompatActivity implements VMEval
         if(item.getItemId() == android.R.id.home){
             finish();
         } else if(item.getItemId() == R.id.action_menu_add_application) {
-            GToast.CreateMessage(Activity_EvaluationList.this, "Add Appplication Under Development", GToast.INFORMATION).show();
+            try {
+                DialogAddApplication loDialog = new DialogAddApplication(Activity_EvaluationList.this);
+                loDialog.initDialog(new DialogAddApplication.OnDialogButtonClickListener() {
+                    @Override
+                    public void OnDownloadClick(Dialog Dialog, String args) {
+                        GToast.CreateMessage(Activity_EvaluationList.this,
+                                "Add Application is under development.",
+                                GToast.INFORMATION).show();
+                    }
+
+                    @Override
+                    public void OnCancel(Dialog Dialog) {
+                        Dialog.dismiss();
+                    }
+                });
+                loDialog.show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         return super.onOptionsItemSelected(item);
     }
