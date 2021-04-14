566f6235446cd847adeab03fe1d8c857a697bb8d
kpdumaguin <dkentpatrickd@gmail.com>
2021-04-14 09:35:40 +0800

    CreditEvaluator - History Log - Created Model for storing data of customer credit evaluation history info.

--- CreditEvaluator/src/main/java/org/rmj/guanzongroup/ghostrider/creditevaluator/Model/EvaluationHistoryInfoModel.java
+++ CreditEvaluator/src/main/java/org/rmj/guanzongroup/ghostrider/creditevaluator/Model/EvaluationHistoryInfoModel.java
@@ -1,6 +1,46 @@
 package org.rmj.guanzongroup.ghostrider.creditevaluator.Model;
 
+import android.view.View;
+
 public class EvaluationHistoryInfoModel {
-    // TODO: Create evaluation history info validations
+    private static final String TAG = EvaluationHistoryInfoModel.class.getSimpleName();
+
+    private final boolean isHeader;
+    private final String psHeader;
+    private final String psLabel;
+    private final String psContent;
+
+    public EvaluationHistoryInfoModel(boolean isHeader, String psHeader, String psLabel, String psContent) {
+        this.isHeader = isHeader;
+        this.psHeader = psHeader;
+        this.psLabel = psLabel;
+        this.psContent = psContent;
+    }
+
+    public int isHeader() {
+        if(isHeader){
+            return View.VISIBLE;
+        }
+        return View.GONE;
+    }
+
+    public int isContent(){
+        if(!isHeader){
+            return View.VISIBLE;
+        }
+        return View.GONE;
+    }
+
+    public String getHeader() {
+        return psHeader;
+    }
+
+    public String getLabel() {
+        return psLabel + " :";
+    }
+
+    public String getContent() {
+        return psContent;
+    }
 
 }
