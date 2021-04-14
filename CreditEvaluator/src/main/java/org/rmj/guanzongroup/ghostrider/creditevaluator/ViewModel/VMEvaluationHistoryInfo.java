8879aa49635c34c6ef2711315d4adcc16d3d5539
kpdumaguin <dkentpatrickd@gmail.com>
2021-04-14 15:00:47 +0800

    CreditEvaluator - History Log - Created needed parameters for ViewModel.

--- CreditEvaluator/src/main/java/org/rmj/guanzongroup/ghostrider/creditevaluator/ViewModel/VMEvaluationHistoryInfo.java
+++ CreditEvaluator/src/main/java/org/rmj/guanzongroup/ghostrider/creditevaluator/ViewModel/VMEvaluationHistoryInfo.java
@@ -5,6 +5,8 @@ import android.os.AsyncTask;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 
 import org.rmj.g3appdriver.GRider.Database.Entities.ECIEvaluation;
 import org.rmj.g3appdriver.GRider.Database.Repositories.RCIEvaluation;
@@ -15,22 +17,36 @@ import java.util.List;
 
 public class VMEvaluationHistoryInfo extends AndroidViewModel {
     private static final String TAG = VMEvaluationHistoryInfo.class.getSimpleName();
+    private final Application instance;
+    private final ECIEvaluation poCredtEv;
     private final RCIEvaluation poInvestx;
+    private final String psTransNo;
 
+    private final MutableLiveData<List<EvaluationHistoryInfoModel>> plDetail = new MutableLiveData<>();
 
-    public VMEvaluationHistoryInfo(@NonNull Application application) {
+    public VMEvaluationHistoryInfo(@NonNull Application application, String fsTransNo, ECIEvaluation foCredtEv) {
         super(application);
+        this.instance = application;
+        this.poCredtEv = foCredtEv;
         this.poInvestx = new RCIEvaluation(application);
+        this.psTransNo = fsTransNo;
+        this.plDetail.setValue(new ArrayList<>());
+        setCreditEvaluationDetl();
     }
 
-    // TODO: Methods for EvaluationHistoryInfo
+    public LiveData<List<EvaluationHistoryInfoModel>> getCreditEvaluationDetl() {
+        return plDetail;
+    }
 
+    private void setCreditEvaluationDetl() {
+        new CustomerEvaluationDetailTask(instance, plDetail::setValue).execute(poCredtEv);
+    }
 
-    private static class CustomerEvaluationInfoTask extends AsyncTask<ECIEvaluation, Void, List<EvaluationHistoryInfoModel>> {
+    private static class CustomerEvaluationDetailTask extends AsyncTask<ECIEvaluation, Void, List<EvaluationHistoryInfoModel>> {
         private final Application instance;
         private final OnFetchCustomerEvaluationInfo pmListener;
 
-        public CustomerEvaluationInfoTask(Application fsInstance, OnFetchCustomerEvaluationInfo fmListener) {
+        public CustomerEvaluationDetailTask(Application fsInstance, OnFetchCustomerEvaluationInfo fmListener) {
             this.instance = fsInstance;
             this.pmListener = fmListener;
         }
