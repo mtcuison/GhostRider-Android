dd7aebdbb80f74f7e0a35971b585510afe752845
kpdumaguin <dkentpatrickd@gmail.com>
2021-04-14 14:08:17 +0800

    CreditEvaluator - History Log - Created AsynTask for retrieving Customer Evaluation Info in VMEvaluationHistoryInfo.java

--- CreditEvaluator/src/main/java/org/rmj/guanzongroup/ghostrider/creditevaluator/ViewModel/VMEvaluationHistoryInfo.java
+++ CreditEvaluator/src/main/java/org/rmj/guanzongroup/ghostrider/creditevaluator/ViewModel/VMEvaluationHistoryInfo.java
@@ -1,11 +1,17 @@
 package org.rmj.guanzongroup.ghostrider.creditevaluator.ViewModel;
 
 import android.app.Application;
+import android.os.AsyncTask;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 
+import org.rmj.g3appdriver.GRider.Database.Entities.ECIEvaluation;
 import org.rmj.g3appdriver.GRider.Database.Repositories.RCIEvaluation;
+import org.rmj.guanzongroup.ghostrider.creditevaluator.Model.EvaluationHistoryInfoModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class VMEvaluationHistoryInfo extends AndroidViewModel {
     private static final String TAG = VMEvaluationHistoryInfo.class.getSimpleName();
@@ -20,11 +26,51 @@ public class VMEvaluationHistoryInfo extends AndroidViewModel {
     // TODO: Methods for EvaluationHistoryInfo
 
 
-    // TODO: AsyncTasks here
+    private static class CustomerEvaluationInfoTask extends AsyncTask<ECIEvaluation, Void, List<EvaluationHistoryInfoModel>> {
+        private final Application instance;
+        private final OnFetchCustomerEvaluationInfo pmListener;
 
+        public CustomerEvaluationInfoTask(Application fsInstance, OnFetchCustomerEvaluationInfo fmListener) {
+            this.instance = fsInstance;
+            this.pmListener = fmListener;
+        }
 
-    // TODO: Interfaces for callbacks
+        @Override
+        protected List<EvaluationHistoryInfoModel> doInBackground(ECIEvaluation... eciEvaluations) {
+            ECIEvaluation loDetail = eciEvaluations[0];
+            List<EvaluationHistoryInfoModel> loListDetl = new ArrayList<>();
+            try {
+                // Headers
+                loListDetl.add(new EvaluationHistoryInfoModel(true, "Residence Information", "", ""));
 
+                loListDetl.add(new EvaluationHistoryInfoModel(true, "Barangay Record & Neighbor's Information", "", ""));
 
+                loListDetl.add(new EvaluationHistoryInfoModel(true, "Disbursement Information", "", ""));
+
+                loListDetl.add(new EvaluationHistoryInfoModel(true, "Character Traits", "", ""));
+
+                // Infos
+                loListDetl.add(new EvaluationHistoryInfoModel(false, "", "Landmark", loDetail.getLandMark()));
+                loListDetl.add(new EvaluationHistoryInfoModel(false, "", "House Ownership", loDetail.getOwnershp()));
+                loListDetl.add(new EvaluationHistoryInfoModel(false ,"", "Households", loDetail.getOwnOther()));
+                loListDetl.add(new EvaluationHistoryInfoModel(false, "", "Type of House", loDetail.getHouseTyp()));
+                loListDetl.add(new EvaluationHistoryInfoModel(false, "", "Has Garage?", loDetail.getGaragexx()));
+
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+            return loListDetl;
+        }
+
+        @Override
+        protected void onPostExecute(List<EvaluationHistoryInfoModel> evaluationInfo) {
+            super.onPostExecute(evaluationInfo);
+            pmListener.onDisplayList(evaluationInfo);
+        }
+    }
+
+    public interface OnFetchCustomerEvaluationInfo {
+        void onDisplayList(List<EvaluationHistoryInfoModel> flDetails);
+    }
 
 }
