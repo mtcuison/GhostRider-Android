098be2b589c3852d8ccd0d5c9ae25212d0d8f5bb
kpdumaguin <dkentpatrickd@gmail.com>
2021-04-20 13:43:32 +0800

    CreditApp - Created class containing constant attrs and methods for Unit Test.

--- creditApp/src/test/java/org/rmj/guanzongroup/onlinecreditapplication/Fragment/Fragment_SpouseSelfEmployedInfoTest.java
+++ creditApp/src/test/java/org/rmj/guanzongroup/onlinecreditapplication/Fragment/Fragment_SpouseSelfEmployedInfoTest.java
@@ -1,13 +1,7 @@
 package org.rmj.guanzongroup.onlinecreditapplication.Fragment;
 
-import android.app.Application;
 import android.os.Build;
-import android.util.Log;
 import android.widget.ArrayAdapter;
-
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-import androidx.test.core.app.ApplicationProvider;
 
 import junit.framework.TestCase;
 
@@ -16,9 +10,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.rmj.g3appdriver.GRider.Database.Entities.ECreditApplicantInfo;
-import org.rmj.guanzongroup.onlinecreditapplication.Etc.CreditAppConstants;
 import org.rmj.guanzongroup.onlinecreditapplication.Model.SpouseSelfEmployedInfoModel;
 import org.rmj.guanzongroup.onlinecreditapplication.Model.ViewModelCallBack;
+import org.rmj.guanzongroup.onlinecreditapplication.TestConstants;
 import org.rmj.guanzongroup.onlinecreditapplication.ViewModel.VMSpouseSelfEmployedInfo;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
@@ -26,75 +20,31 @@ import org.robolectric.annotation.Config;
 @RunWith(RobolectricTestRunner.class)
 @Config(sdk = {Build.VERSION_CODES.O_MR1}, manifest = Config.NONE)
 public class Fragment_SpouseSelfEmployedInfoTest extends TestCase {
-    private static final Application APPLICATION = ApplicationProvider.getApplicationContext();
-    private static final String TRANSACTION_NO = "M021210014";
-    private static final String STRING_ZERO = "0";
-    private static final String STRING_ONE = "1";
-    private static final String STRING_TWO = "2";
-    private static final String FAKE_STRING = "Fake String";
-    private static final String FAKE_JSON = "{\"key\":\"value\"}";
-    private static final String FAKE_CODE = "00123";
-    private static final String FAKE_NUMBER = "0123456789";
-    private static final String FAKE_AMOUNT = "50,000";
-    private static final String FAKE_COMPANY = "Guanzon Group of Companies";
     private SpouseSelfEmployedInfoModel infoModel;
     private VMSpouseSelfEmployedInfo mViewModel;
+    private ECreditApplicantInfo poInfo;
 
-    @Mock
-    ECreditApplicantInfo poInfo;
+//    @Mock
+//    ECreditApplicantInfo poInfo;
 
     @Before
     public void setUp() {
-        poInfo = new ECreditApplicantInfo();
+        poInfo = TestConstants.getDummyCreditApp();
         infoModel = new SpouseSelfEmployedInfoModel();
-        mViewModel = new VMSpouseSelfEmployedInfo(APPLICATION);
+        mViewModel = new VMSpouseSelfEmployedInfo(TestConstants.APPLICATION);
 
-        poInfo.setTransNox(TRANSACTION_NO);
-        poInfo.setClientNm(FAKE_NUMBER);
-        poInfo.setDetlInfo(FAKE_JSON);
-        poInfo.setPurchase(FAKE_STRING);
-        poInfo.setApplInfo(FAKE_JSON);
-        poInfo.setResidnce(FAKE_JSON);
-        poInfo.setAppMeans(FAKE_JSON);
-        poInfo.setEmplymnt(FAKE_JSON);
-        poInfo.setBusnInfo(FAKE_JSON);
-        poInfo.setFinancex(FAKE_JSON);
-        poInfo.setPensionx(FAKE_JSON);
-        poInfo.setOtherInc(FAKE_JSON);
-        poInfo.setSpousexx(FAKE_JSON);
-        poInfo.setSpsResdx(FAKE_JSON);
-        poInfo.setSpsMeans(FAKE_JSON);
-        poInfo.setSpsEmplx(FAKE_JSON);
-        poInfo.setSpsBusnx(FAKE_JSON);
-        poInfo.setSpsPensn(FAKE_JSON);
-        poInfo.setDisbrsmt(FAKE_JSON);
-        poInfo.setDependnt(FAKE_JSON);
-        poInfo.setProperty(FAKE_JSON);
-        poInfo.setOthrInfo(FAKE_JSON);
-        poInfo.setComakerx(FAKE_JSON);
-        poInfo.setCmResidx(FAKE_JSON);
-        poInfo.setIsSpouse(STRING_ONE);
-        poInfo.setIsComakr(STRING_ONE);
-        poInfo.setBranchCd(FAKE_CODE);
-        poInfo.setAppliedx(STRING_ONE);
-        poInfo.setTransact("01-01-1990");
-        poInfo.setCreatedx("01-01-1990");
-        poInfo.setDownPaym(12.0);
-        poInfo.setTranStat(STRING_ZERO);
-
-
-        mViewModel.setTransNox(TRANSACTION_NO);
+        mViewModel.setTransNox(TestConstants.TRANSACTION_NO);
         mViewModel.setDetailInfo(poInfo);
-        mViewModel.setBizIndustry(STRING_ONE);
-        infoModel.setsBizName(FAKE_COMPANY);
-        infoModel.setsBizAddress(FAKE_CODE);
-        mViewModel.setProvinceID(FAKE_CODE);
-        mViewModel.setTownID(FAKE_CODE);
-        mViewModel.setBizType(STRING_ZERO);
-        mViewModel.setBizSize(STRING_TWO);
-        infoModel.setsBizYrs(STRING_TWO);
-        mViewModel.setMosOrYr(STRING_ZERO);
-        infoModel.setsGrossMonthly(FAKE_AMOUNT);
+        mViewModel.setBizIndustry(TestConstants.STRING_ONE);
+        infoModel.setsBizName(TestConstants.FAKE_COMPANY);
+        infoModel.setsBizAddress(TestConstants.FAKE_CODE);
+        mViewModel.setProvinceID(TestConstants.FAKE_CODE);
+        mViewModel.setTownID(TestConstants.FAKE_CODE);
+        mViewModel.setBizType(TestConstants.STRING_ZERO);
+        mViewModel.setBizSize(TestConstants.STRING_TWO);
+        infoModel.setsBizYrs(TestConstants.STRING_TWO);
+        mViewModel.setMosOrYr(TestConstants.STRING_ZERO);
+        infoModel.setsGrossMonthly(TestConstants.FAKE_STRING_AMOUNT);
         infoModel.setsMonthlyExps("25,000");
     }
 
@@ -105,32 +55,32 @@ public class Fragment_SpouseSelfEmployedInfoTest extends TestCase {
 
     @Test
     public void testSetBizIndustry() {
-        assertTrue(mViewModel.setBizIndustry(STRING_ONE));
+        assertTrue(mViewModel.setBizIndustry(TestConstants.STRING_ONE));
     }
 
     @Test
     public void testSetBizType() {
-        assertTrue(mViewModel.setBizType(STRING_ZERO));
+        assertTrue(mViewModel.setBizType(TestConstants.STRING_ZERO));
     }
 
     @Test
     public void testSetBizSize() {
-        assertTrue(mViewModel.setBizSize(STRING_TWO));
+        assertTrue(mViewModel.setBizSize(TestConstants.STRING_TWO));
     }
 
     @Test
     public void testSetMosOrYr() {
-        assertTrue(mViewModel.setMosOrYr(STRING_ZERO));
+        assertTrue(mViewModel.setMosOrYr(TestConstants.STRING_ZERO));
     }
 
     @Test
     public void testSetProvinceId() {
-        assertTrue(mViewModel.setProvinceID(FAKE_CODE));
+        assertTrue(mViewModel.setProvinceID(TestConstants.FAKE_CODE));
     }
 
     @Test
     public void testSetTownId() {
-        assertTrue(mViewModel.setTownID(FAKE_CODE));
+        assertTrue(mViewModel.setTownID(TestConstants.FAKE_CODE));
     }
 
     @Test
---
+++ creditApp/src/test/java/org/rmj/guanzongroup/onlinecreditapplication/TestConstants.java
@@ -0,0 +1,61 @@
+package org.rmj.guanzongroup.onlinecreditapplication;
+
+import android.app.Application;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import org.rmj.g3appdriver.GRider.Database.Entities.ECreditApplicantInfo;
+
+public class TestConstants {
+    public static final Application APPLICATION = ApplicationProvider.getApplicationContext();
+    public static final String TRANSACTION_NO = "M021210014";
+    public static final String STRING_ZERO = "0";
+    public static final String STRING_ONE = "1";
+    public static final String STRING_TWO = "2";
+    public static final String FAKE_STRING = "Fake String";
+    public static final String FAKE_JSON = "{\"key\":\"value\"}";
+    public static final String FAKE_CODE = "00123";
+    public static final String FAKE_NUMBER = "0123456789";
+    public static final String FAKE_STRING_AMOUNT = "50,000";
+    public static final String FAKE_DATE = "01-01-1990";
+    public static final String FAKE_COMPANY = "Guanzon Group of Companies";
+    public static final double FAKE_DBLE_AMOUNT = 20000.00;
+
+    public static ECreditApplicantInfo getDummyCreditApp() {
+        ECreditApplicantInfo loCredit = new ECreditApplicantInfo();
+        loCredit.setTransNox(TRANSACTION_NO);
+        loCredit.setClientNm(FAKE_NUMBER);
+        loCredit.setDetlInfo(FAKE_JSON);
+        loCredit.setPurchase(FAKE_STRING);
+        loCredit.setApplInfo(FAKE_JSON);
+        loCredit.setResidnce(FAKE_JSON);
+        loCredit.setAppMeans(FAKE_JSON);
+        loCredit.setEmplymnt(FAKE_JSON);
+        loCredit.setBusnInfo(FAKE_JSON);
+        loCredit.setFinancex(FAKE_JSON);
+        loCredit.setPensionx(FAKE_JSON);
+        loCredit.setOtherInc(FAKE_JSON);
+        loCredit.setSpousexx(FAKE_JSON);
+        loCredit.setSpsResdx(FAKE_JSON);
+        loCredit.setSpsMeans(FAKE_JSON);
+        loCredit.setSpsEmplx(FAKE_JSON);
+        loCredit.setSpsBusnx(FAKE_JSON);
+        loCredit.setSpsPensn(FAKE_JSON);
+        loCredit.setDisbrsmt(FAKE_JSON);
+        loCredit.setDependnt(FAKE_JSON);
+        loCredit.setProperty(FAKE_JSON);
+        loCredit.setOthrInfo(FAKE_JSON);
+        loCredit.setComakerx(FAKE_JSON);
+        loCredit.setCmResidx(FAKE_JSON);
+        loCredit.setIsSpouse(STRING_ONE);
+        loCredit.setIsComakr(STRING_ONE);
+        loCredit.setBranchCd(FAKE_CODE);
+        loCredit.setAppliedx(STRING_ONE);
+        loCredit.setTransact(FAKE_DATE);
+        loCredit.setCreatedx(FAKE_DATE);
+        loCredit.setDownPaym(FAKE_DBLE_AMOUNT);
+        loCredit.setTranStat(STRING_ZERO);
+        return loCredit;
+    }
+
+}
