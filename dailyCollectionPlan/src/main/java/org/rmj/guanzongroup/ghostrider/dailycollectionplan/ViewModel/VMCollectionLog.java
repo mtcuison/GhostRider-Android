b4b0f3d8ee8267c3db0a80af8b33f26cd3ba2f0a
mikegarcia8748 <mikegarcia8748@gmail.com>
2021-04-21 08:27:29 +0800

    Revision of Activity_CollectionRemittance.java From viewing Total Collection Detail to UI and Presenter for collection Remittance.
    Revision of DialogRemitCollection.java from UI and presenter for remittance to dialog for total collection detail.
    Added Location coordinates API reporting in WebApi.java
    Remove AsynckTask for collection remittance in VMCollectionLog.java
    Added Employee Level on DeptCode.java

--- dailyCollectionPlan/src/main/java/org/rmj/guanzongroup/ghostrider/dailycollectionplan/ViewModel/VMCollectionLog.java
+++ dailyCollectionPlan/src/main/java/org/rmj/guanzongroup/ghostrider/dailycollectionplan/ViewModel/VMCollectionLog.java
@@ -2,16 +2,12 @@ package org.rmj.guanzongroup.ghostrider.dailycollectionplan.ViewModel;
 
 import android.annotation.SuppressLint;
 import android.app.Application;
-import android.os.AsyncTask;
-import android.os.Build;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
-import org.json.JSONObject;
 import org.rmj.g3appdriver.GRider.Constants.AppConstants;
 import org.rmj.g3appdriver.GRider.Database.DataAccessObject.DDCPCollectionDetail;
 import org.rmj.g3appdriver.GRider.Database.Entities.EAddressUpdate;
@@ -19,7 +15,6 @@ import org.rmj.g3appdriver.GRider.Database.Entities.EBankInfo;
 import org.rmj.g3appdriver.GRider.Database.Entities.EBranchInfo;
 import org.rmj.g3appdriver.GRider.Database.Entities.EDCPCollectionDetail;
 import org.rmj.g3appdriver.GRider.Database.Entities.EDCPCollectionMaster;
-import org.rmj.g3appdriver.GRider.Database.Entities.EDCP_Remittance;
 import org.rmj.g3appdriver.GRider.Database.Entities.EImageInfo;
 import org.rmj.g3appdriver.GRider.Database.Entities.EMobileUpdate;
 import org.rmj.g3appdriver.GRider.Database.Repositories.RBankInfo;
@@ -28,10 +23,6 @@ import org.rmj.g3appdriver.GRider.Database.Repositories.RCollectionUpdate;
 import org.rmj.g3appdriver.GRider.Database.Repositories.RDCP_Remittance;
 import org.rmj.g3appdriver.GRider.Database.Repositories.RDailyCollectionPlan;
 import org.rmj.g3appdriver.GRider.Database.Repositories.RImageInfo;
-import org.rmj.g3appdriver.GRider.Http.HttpHeaders;
-import org.rmj.g3appdriver.GRider.Http.WebClient;
-import org.rmj.g3appdriver.utils.ConnectionUtil;
-import org.rmj.g3appdriver.utils.WebApi;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -47,8 +38,6 @@ public class VMCollectionLog extends AndroidViewModel {
     private final RDCP_Remittance poRemit;
     private final RCollectionUpdate poUpdate;
     private final RBankInfo poBank;
-    private final ConnectionUtil poConn;
-    private final HttpHeaders poHeaders;
 
     private final MutableLiveData<EDCPCollectionMaster> poMaster = new MutableLiveData<>();
     private final MutableLiveData<List<EImageInfo>> plImageLst = new MutableLiveData<>();
@@ -71,8 +60,6 @@ public class VMCollectionLog extends AndroidViewModel {
         this.dTransact.setValue(AppConstants.CURRENT_DATE);
         this.poRemit = new RDCP_Remittance(application);
         this.poBank = new RBankInfo(application);
-        this.poConn = new ConnectionUtil(application);
-        this.poHeaders = HttpHeaders.getInstance(application);
     }
 
     public LiveData<EBranchInfo> getUserBranchInfo(){
@@ -178,83 +165,5 @@ public class VMCollectionLog extends AndroidViewModel {
 
     public void setMobileList(List<EMobileUpdate> paMobile) {
         this.paMobile.setValue(paMobile);
-    }
-
-
-    public void RemitCollection(EDCP_Remittance foRemittance, VMCollectionRemittance.OnRemitCollectionCallback callback){
-        new RemitCollectionTask(callback).execute(foRemittance);
-    }
-
-    @SuppressLint("StaticFieldLeak")
-    private class RemitCollectionTask extends AsyncTask<EDCP_Remittance, Void, String> {
-
-        VMCollectionRemittance.OnRemitCollectionCallback callback;
-
-        public RemitCollectionTask(VMCollectionRemittance.OnRemitCollectionCallback callback) {
-            this.callback = callback;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            callback.OnRemit();
-        }
-
-        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-        @Override
-        protected String doInBackground(EDCP_Remittance... edcp_remittances) {
-            String lsResult = "";
-            EDCP_Remittance loRemit = edcp_remittances[0];
-            try{
-                String lsTransNox = poRemit.getTransnoxMaster(loRemit.getTransact());
-                String lsEntryNox = poRemit.getRemittanceEntry(loRemit.getTransact());
-                loRemit.setTransNox(lsTransNox);
-                loRemit.setEntryNox(lsEntryNox);
-                poRemit.insert(loRemit);
-                if(!poConn.isDeviceConnected()){
-                    lsResult = AppConstants.LOCAL_EXCEPTION_ERROR("Cash Remittance has been save.");
-                } else {
-                    JSONObject param = new JSONObject();
-                    param.put("sTransNox", loRemit.getTransNox());
-                    param.put("nEntryNox", 2);
-                    param.put("dTransact", loRemit.getTransact());
-                    param.put("cRemitTyp", loRemit.getRemitTyp());
-                    param.put("sCompnyNm", loRemit.getCompnyNm());
-                    param.put("sBankAcct", loRemit.getBankAcct());
-                    param.put("sReferNox", loRemit.getReferNox());
-                    param.put("cPaymType", loRemit.getPaymForm());
-                    param.put("nAmountxx", Double.parseDouble(loRemit.getAmountxx()));
-
-                    String lsResponse = WebClient.httpsPostJSon(WebApi.URL_DCP_REMITTANCE, param.toString(), poHeaders.getHeaders());
-                    JSONObject loJson = new JSONObject(lsResponse);
-                    String result = loJson.getString("result");
-                    if(result.equalsIgnoreCase("success")){
-                        poRemit.updateSendStat(loRemit.getTransNox(), loRemit.getEntryNox());
-                    }
-                    lsResult = lsResponse;
-                }
-            } catch (Exception e){
-                e.printStackTrace();
-            }
-            return lsResult;
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            try{
-                JSONObject loJson = new JSONObject(s);
-                String lsResult = loJson.getString("result");
-                if(lsResult.equalsIgnoreCase("success")){
-                    callback.OnSuccess();
-                } else {
-                    JSONObject loError = loJson.getJSONObject("error");
-                    String message = loError.getString("message");
-                    callback.OnFailed(message);
-                }
-            } catch (Exception e){
-                e.printStackTrace();
-            }
-        }
     }
 }
