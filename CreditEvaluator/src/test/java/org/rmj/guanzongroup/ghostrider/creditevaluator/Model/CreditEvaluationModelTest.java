c5b450e1d99eb7286473b34bad137a76ce82d014
unclejo <jonsabiniano03@gmail.com>
2021-04-22 11:01:12 +0800

    Update bug saving Neighbor information in Barangay Record

---
+++ CreditEvaluator/src/test/java/org/rmj/guanzongroup/ghostrider/creditevaluator/Model/CreditEvaluationModelTest.java
@@ -0,0 +1,29 @@
+package org.rmj.guanzongroup.ghostrider.creditevaluator.Model;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class CreditEvaluationModelTest {
+    private String sTransNox;
+    private String dTransact;
+    private String sCredInvx;
+    private String sCompnyNm;
+    private String sSpouseNm;
+    private String sAddressx;
+    private String sMobileNo;
+    private String sQMAppCde;
+    private String sModelNme;
+    private String nDownPaym;
+    private String nAcctTerm;
+    private String cTranStat;
+    private String dTimeStmp;
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
