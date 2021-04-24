afbcfbbfc7093682fcc4641fe4be31097dd9c902
mikegarcia8748 <mikegarcia8748@gmail.com>
2021-04-24 15:31:19 +0800

    Added copyright comment to all class

--- creditApp/src/main/java/org/rmj/guanzongroup/onlinecreditapplication/ViewModel/VMSpouseResidenceInfo.java
+++ creditApp/src/main/java/org/rmj/guanzongroup/onlinecreditapplication/ViewModel/VMSpouseResidenceInfo.java
@@ -1,4 +1,15 @@
-    package org.rmj.guanzongroup.onlinecreditapplication.ViewModel;
+    /*
+ * Created by Android Team MIS-SEG Year 2021
+ * Copyright (c) 2021. Guanzon Central Office
+ * Guanzon Bldg., Perez Blvd., Dagupan City, Pangasinan 2400
+ * Project name : GhostRider_Android
+ * Module : GhostRider_Android.creditApp
+ * Electronic Personnel Access Control Security System
+ * project file created : 4/24/21 3:19 PM
+ * project file last modified : 4/24/21 3:17 PM
+ */
+
+package org.rmj.guanzongroup.onlinecreditapplication.ViewModel;
 
     import android.app.Application;
     import android.util.Log;
--- griderScanner/src/main/java/org/rmj/guanzongroup/ghostrider/griderscanner/helpers/MathUtils.java
+++ griderScanner/src/main/java/org/rmj/guanzongroup/ghostrider/griderscanner/helpers/MathUtils.java
@@ -1,10 +1,12 @@
 /*
- * *
- *  * Created by Ali YÜCE on 3/2/20 11:18 PM
- *  * https://github.com/mayuce/
- *  * Copyright (c) 2020 . All rights reserved.
- *  * Last modified 3/2/20 11:10 PM
- *
+ * Created by Android Team MIS-SEG Year 2021
+ * Copyright (c) 2021. Guanzon Central Office
+ * Guanzon Bldg., Perez Blvd., Dagupan City, Pangasinan 2400
+ * Project name : GhostRider_Android
+ * Module : GhostRider_Android.griderScanner
+ * Electronic Personnel Access Control Security System
+ * project file created : 4/24/21 3:19 PM
+ * project file last modified : 4/24/21 3:18 PM
  */
 
 package org.rmj.guanzongroup.ghostrider.griderscanner.helpers;
