cad39f8478d65d8e7c74a42dff55b79bd95d65e3
kpdumaguin <dkentpatrickd@gmail.com>
2021-05-20 11:40:04 +0800

    CI - Created unit test For Evaluation History Info

---
+++ CreditEvaluator/src/test/java/org/rmj/guanzongroup/ghostrider/creditevaluator/CiTestConstants.java
@@ -0,0 +1,21 @@
+/*
+ * Created by Android Team MIS-SEG Year 2021
+ * Copyright (c) 2021. Guanzon Central Office
+ * Guanzon Bldg., Perez Blvd., Dagupan City, Pangasinan 2400
+ * Project name : GhostRider_Android
+ * Module : GhostRider_Android.CreditEvaluator
+ * Electronic Personnel Access Control Security System
+ * project file created : 5/20/21 10:57 AM
+ * project file last modified : 5/20/21 10:57 AM
+ */
+
+package org.rmj.guanzongroup.ghostrider.creditevaluator;
+
+import android.app.Application;
+
+import androidx.test.core.app.ApplicationProvider;
+
+public class CiTestConstants {
+    public static final Application APPLICATION = ApplicationProvider.getApplicationContext();
+    public static final String TRANSACTION_NO = "M00121000001";
+}
