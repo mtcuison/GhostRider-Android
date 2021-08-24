203f8751282c895ae5ce3b02201a2db43c900043
unclejo <jonsabiniano03@gmail.com>
2021-08-24 16:01:26 +0800

    add download pdf from link add displaying pdf

--- notifications/src/main/java/org/rmj/guanzongroup/ghostrider/notifications/Fragment/Fragment_ViewNotification.java
+++ notifications/src/main/java/org/rmj/guanzongroup/ghostrider/notifications/Fragment/Fragment_ViewNotification.java
@@ -16,6 +16,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -24,6 +25,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.text.Html;
+import android.text.Spannable;
+import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
@@ -38,12 +41,14 @@ import android.widget.Toast;
 import com.google.android.material.button.MaterialButton;
 
 import org.rmj.g3appdriver.GRider.Etc.FormatUIText;
+import org.rmj.g3appdriver.GRider.Etc.GToast;
 import org.rmj.g3appdriver.GRider.Etc.LoadDialog;
 import org.rmj.g3appdriver.GRider.Etc.MessageBox;
 import org.rmj.guanzongroup.ghostrider.ahmonitoring.Activity.Activity_CashCounter;
 import org.rmj.guanzongroup.ghostrider.notifications.Activity.Activity_Notifications;
 import org.rmj.guanzongroup.ghostrider.notifications.Dialog.Dialog_ReplyNotification;
 import org.rmj.guanzongroup.ghostrider.notifications.R;
+import org.rmj.guanzongroup.ghostrider.notifications.ViewModel.VMNotificationList;
 import org.rmj.guanzongroup.ghostrider.notifications.ViewModel.VMViewNotification;
 
 public class Fragment_ViewNotification extends Fragment {
@@ -75,7 +80,7 @@ public class Fragment_ViewNotification extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(VMViewNotification.class);
         poMsgBox = new MessageBox(getActivity());
-
+        StringUrl();
         btnReply.setOnClickListener(v -> {
             Dialog_ReplyNotification loDialog = new Dialog_ReplyNotification(getContext(), Activity_Notifications.getInstance().getSender());
             loDialog.initDialog(new Dialog_ReplyNotification.OnDialogButtonClickListener() {
@@ -125,97 +130,92 @@ public class Fragment_ViewNotification extends Fragment {
 
     private void setContent(View v) {
         try {
-            Log.e("SAmple", Activity_Notifications.getInstance().getMessage());
             title.setText(Activity_Notifications.getInstance().getMessageTitle());
             sender.setText(Activity_Notifications.getInstance().getSender());
             recepient.setText(Activity_Notifications.getInstance().getReceipt());
             date.setText(FormatUIText.getParseDateTime(Activity_Notifications.getInstance().getDate()));
-            setTextViewHTML(message, Activity_Notifications.getInstance().getMessage());
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            message.setText(Activity_Notifications.getInstance().getMessage());
+//            setTextViewHTML(message, Activity_Notifications.getInstance().getMessage());
 
-//                message.setText(Html.fromHtml(Activity_Notifications.getInstance().getMessage(), Html.FROM_HTML_MODE_COMPACT));
-//            } else {
-//                setTextViewHTML(message, String.valueOf(Html.fromHtml(Activity_Notifications.getInstance().getMessage())));
-//                message.setText(Html.fromHtml(Activity_Notifications.getInstance().getMessage()));
-//            }
-//            message.setMovementMethod(LinkMovementMethod.getInstance());
-
-
-            requestCashCount(v, Activity_Notifications.getInstance().getMsgType());
+//
+//            requestCashCount(v, Activity_Notifications.getInstance().getMsgType());
 
         } catch (NullPointerException e) {
             e.printStackTrace();
         }
     }
-    protected void makeLinkClickable(SpannableStringBuilder strBuilder, final URLSpan span)
-    {
-        int start = strBuilder.getSpanStart(span);
-        int end = strBuilder.getSpanEnd(span);
-        int flags = strBuilder.getSpanFlags(span);
-        ClickableSpan clickable = new ClickableSpan() {
-            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-            public void onClick(View view) {
-                // Do something with span.getURL() to handle the link click...
-                Log.e("Notification", String.valueOf(span.getURL()));
-
-//                PDFTools.showPDFUrl(getActivity(), span.getURL());
-                mViewModel.DownloadPDF(span.getURL(), new VMViewNotification.onDownLoadPDF() {
-                    @Override
-                    public void OnDownloadPDF(String title, String message) {
-                        poProgress.initDialog(title, message, false);
-                        poProgress.show();
-                    }
-
-                    @Override
-                    public void OnFinishDownload(Intent intent) {
-                        poProgress.dismiss();
-                        requireActivity().startActivity(intent);
-                    }
-
+//    protected void makeLinkClickable(SpannableStringBuilder strBuilder, final URLSpan span)
+//    {
+//        int start = strBuilder.getSpanStart(span);
+//        int end = strBuilder.getSpanEnd(span);
+//        int flags = strBuilder.getSpanFlags(span);
+//        ClickableSpan clickable = new ClickableSpan() {
+//            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+//            public void onClick(View view) {
+//                // Do something with span.getURL() to handle the link click...
+//                Log.e("Notification url", String.valueOf(span.getURL()));
+//
+////                PDFTools.showPDFUrl(getActivity(), span.getURL());
+//                mViewModel.DownloadPDF(span.getURL(), new VMViewNotification.onDownLoadPDF() {
+//                    @Override
+//                    public void OnDownloadPDF(String title, String message) {
+//                        poProgress.initDialog(title, message, false);
+//                        poProgress.show();
+//                    }
+//
 //                    @Override
 //                    public void OnFinishDownload(Intent intent) {
 //                        poProgress.dismiss();
-//                        GToast.CreateMessage(getActivity(), "Download Success.",GToast.INFORMATION).show();
-//                        startActivity(intent);
+//                        requireActivity().startActivity(intent);
 //                    }
-
-                    @Override
-                    public void OnFailedDownload(String message) {
-                        poProgress.dismiss();
-                        initErrorDialog("Notification", message);
-                    }
-                });
-            }
-        };
-        strBuilder.setSpan(clickable, start, end, flags);
-        strBuilder.removeSpan(span);
-    }
-
-    protected void setTextViewHTML(TextView text, String html)
-    {
-        CharSequence sequence = Html.fromHtml(html);
-        SpannableStringBuilder strBuilder = new SpannableStringBuilder(sequence);
-        URLSpan[] urls = strBuilder.getSpans(0, sequence.length(), URLSpan.class);
-        for(URLSpan span : urls) {
-            makeLinkClickable(strBuilder, span);
-        }
-        text.setText(strBuilder);
-        text.setMovementMethod(LinkMovementMethod.getInstance());
-    }
-    private void requestCashCount(View v, String fsMsgType) {
-        if(!fsMsgType.equalsIgnoreCase("00003")) {
-            return;
-        } else {
-            MaterialButton btnCashCnt = v.findViewById(R.id.btn_cash_count);
-            btnCashCnt.setVisibility(View.VISIBLE);
-            btnCashCnt.setOnClickListener(view -> {
-                Intent loIntent = new Intent(getActivity(), Activity_CashCounter.class);
-                loIntent.putExtra("sTransNox","1122");
-                startActivity(loIntent);
-                getActivity().finish();
-            });
-        }
-    }
+//
+////                    @Override
+////                    public void OnFinishDownload(Intent intent) {
+////                        poProgress.dismiss();
+////                        GToast.CreateMessage(getActivity(), "Download Success.",GToast.INFORMATION).show();
+////                        startActivity(intent);
+////                    }
+//
+//                    @Override
+//                    public void OnFailedDownload(String message) {
+//                        poProgress.dismiss();
+//                        initErrorDialog("Notification", message);
+//                    }
+//                });
+//            }
+//        };
+//        strBuilder.setSpan(clickable, start, end, flags);
+//        strBuilder.removeSpan(span);
+//    }
+//
+//    protected void setTextViewHTML(TextView text, String html)
+//    {
+//        CharSequence sequence = html;
+//        SpannableStringBuilder strBuilder = new SpannableStringBuilder(sequence);
+//        URLSpan[] urls = strBuilder.getSpans(0, sequence.length(), URLSpan.class);
+//        for(URLSpan span : urls) {
+//            makeLinkClickable(strBuilder, span);
+//            Log.e("TAG", span.getURL());
+//        }
+//
+//        text.setLinksClickable(true);
+//        text.setText(strBuilder);
+//        text.setMovementMethod(LinkMovementMethod.getInstance());
+//    }
+//    private void requestCashCount(View v, String fsMsgType) {
+//        if(!fsMsgType.equalsIgnoreCase("00003")) {
+//            return;
+//        } else {
+//            MaterialButton btnCashCnt = v.findViewById(R.id.btn_cash_count);
+//            btnCashCnt.setVisibility(View.VISIBLE);
+//            btnCashCnt.setOnClickListener(view -> {
+//                Intent loIntent = new Intent(getActivity(), Activity_CashCounter.class);
+//                loIntent.putExtra("sTransNox","1122");
+//                startActivity(loIntent);
+//                getActivity().finish();
+//            });
+//        }
+//    }
     public void initErrorDialog(String title, String message){
         loMessage.initDialog();
         loMessage.setTitle(title);
@@ -224,7 +224,50 @@ public class Fragment_ViewNotification extends Fragment {
                 dialog.dismiss());
         loMessage.show();
     }
+    public void StringUrl(){
 
+        SpannableString current = (SpannableString) message.getText();
+        URLSpan[] spans = current.getSpans(0, current.length(), URLSpan.class);
 
+        for (URLSpan span : spans) {
+            int start = current.getSpanStart(span);
+            int end = current.getSpanEnd(span);
+            current.removeSpan(span);
+            current.setSpan(new MyUrlSpan(span.getURL(), mViewModel), start, end, 0);
+        }
+    }
+    class MyUrlSpan extends URLSpan  {
 
+        private VMViewNotification mViewModel;
+
+        public MyUrlSpan(String url,VMViewNotification mViewModel) {
+            super(url);
+            this.mViewModel = mViewModel;
+
+        }
+        @Override
+        public void onClick(@NonNull View widget) {
+            Uri uri = Uri.parse(getURL());
+            Log.e("URL", uri.toString());
+            mViewModel.DownloadPDF(uri.toString(), new VMViewNotification.onDownLoadPDF() {
+                @Override
+                public void OnDownloadPDF(String title, String message) {
+                    poProgress.initDialog(title, message, false);
+                    poProgress.show();
+                }
+
+                @Override
+                public void OnFinishDownload(Intent intent) {
+                    poProgress.dismiss();
+                    requireActivity().startActivity(intent);
+                }
+
+                @Override
+                public void OnFailedDownload(String message) {
+                    poProgress.dismiss();
+                    initErrorDialog("Notification", message);
+                }
+            });
+        }
+    }
 }
