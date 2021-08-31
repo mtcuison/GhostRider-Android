25705a6168587b57c357b330109524ba60e1a4c5
unclejo <jonsabiniano03@gmail.com>
2021-08-31 09:05:40 +0800

    Credit Application refactor code for gocas null value

---
+++ creditApp/src/main/java/org/rmj/guanzongroup/onlinecreditapplication/Activity/Activity_CreditAppHome.java
@@ -0,0 +1,27 @@
+/*
+ * Created by Android Team MIS-SEG Year 2021
+ * Copyright (c) 2021. Guanzon Central Office
+ * Guanzon Bldg., Perez Blvd., Dagupan City, Pangasinan 2400
+ * Project name : GhostRider_Android
+ * Module : GhostRider_Android.creditApp
+ * Electronic Personnel Access Control Security System
+ * project file created : 8/31/21 8:28 AM
+ * project file last modified : 8/31/21 8:26 AM
+ */
+
+package org.rmj.guanzongroup.onlinecreditapplication.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.rmj.guanzongroup.onlinecreditapplication.R;
+
+public class Activity_CreditAppHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credit_app_home);
+    }
+}
