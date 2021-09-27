8cadb23461294aae1f3996a28600d63b0ae51633
unclejo <jonsabiniano03@gmail.com>
2021-09-27 11:25:57 +0800

    Update CreditApp setting field value if exist in local database

--- creditApp/src/main/java/org/rmj/guanzongroup/onlinecreditapplication/Fragment/Fragment_MeansInfoSelection.java
+++ creditApp/src/main/java/org/rmj/guanzongroup/onlinecreditapplication/Fragment/Fragment_MeansInfoSelection.java
@@ -11,8 +11,10 @@
 
 package org.rmj.guanzongroup.onlinecreditapplication.Fragment;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +27,9 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.rmj.g3appdriver.GRider.Database.Entities.ECreditApplicantInfo;
 import org.rmj.g3appdriver.GRider.Etc.GToast;
 import org.rmj.g3appdriver.GRider.Etc.MessageBox;
 import org.rmj.gocas.pojo.ApplicantInfo;
@@ -37,6 +42,7 @@ import org.rmj.guanzongroup.onlinecreditapplication.ViewModel.VMMeansInfoSelecti
 
 public class Fragment_MeansInfoSelection extends Fragment implements ViewModelCallBack {
 
+    private static final String TAG = Fragment_MeansInfoSelection.class.getSimpleName();
     private VMMeansInfoSelection.MeansInfo infoModel;
     private VMMeansInfoSelection mViewModel;
 
@@ -82,7 +88,11 @@ public class Fragment_MeansInfoSelection extends Fragment implements ViewModelCa
         mViewModel = new ViewModelProvider(this).get(VMMeansInfoSelection.class);
         mViewModel.setTransNox(TransNox);
         
-        mViewModel.getCreditApplicantInfo(TransNox).observe(getViewLifecycleOwner(), eCreditApplicantInfo -> mViewModel.setGOCasDetailInfo(eCreditApplicantInfo));
+        mViewModel.getCreditApplicantInfo(TransNox).observe(getViewLifecycleOwner(), eCreditApplicantInfo -> {
+            mViewModel.setGOCasDetailInfo(eCreditApplicantInfo);
+            setFieldValues(eCreditApplicantInfo);
+        });
+
         btnNext.setOnClickListener(view -> {
 //            try {
 ////                String personal_info = Activity_CreditApplication.getInstance().getApplicantInfo().toJSONString();
@@ -128,6 +138,47 @@ public class Fragment_MeansInfoSelection extends Fragment implements ViewModelCa
             }).show();
         } else {
             GToast.CreateMessage(getActivity(), message, GToast.ERROR).show();
+        }
+    }
+    @SuppressLint("NewApi")
+    private void setFieldValues(ECreditApplicantInfo foCredApp){
+        if (foCredApp.getAppMeans() != null) {
+            try {
+                JSONObject loJson = new JSONObject(foCredApp.getAppMeans());
+                Log.e(TAG + " jsonCon", loJson.toString());
+                infoModel = new VMMeansInfoSelection.MeansInfo();
+                if(loJson.getString("employed").equalsIgnoreCase("1")){
+                    cbEmployed.setChecked(true);
+                    infoModel.setEmployed("1");
+                }else{
+                    cbEmployed.setChecked(false);
+                    infoModel.setEmployed("0");
+                }
+
+                if(loJson.getString("sEmplyed").equalsIgnoreCase("1")){
+                    cbSEmployd.setChecked(true);
+                    infoModel.setSelfEmployed("1");
+                }else{
+                    cbSEmployd.setChecked(false);
+                    infoModel.setSelfEmployed("0");
+                }
+                if(loJson.getString("financer").equalsIgnoreCase("1")){
+                    cbFinancex.setChecked(true);
+                    infoModel.setFinance("1");
+                }else{
+                    cbFinancex.setChecked(false);
+                    infoModel.setFinance("0");
+                }
+                if(loJson.getString("pensionx").equalsIgnoreCase("1")){
+                    cbPensionx.setChecked(true);
+                    infoModel.setPension("1");
+                }else{
+                    cbPensionx.setChecked(false);
+                    infoModel.setPension("0");
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- creditApp/src/main/java/org/rmj/guanzongroup/onlinecreditapplication/ViewModel/VMReviewLoanApp.java
+++ creditApp/src/main/java/org/rmj/guanzongroup/onlinecreditapplication/ViewModel/VMReviewLoanApp.java
@@ -31,6 +31,7 @@ import org.rmj.g3appdriver.GRider.Database.Entities.EImageInfo;
 import org.rmj.g3appdriver.GRider.Database.Entities.EMcBrand;
 import org.rmj.g3appdriver.GRider.Database.Entities.EMcModel;
 import org.rmj.g3appdriver.GRider.Database.Repositories.RBarangay;
+import org.rmj.g3appdriver.GRider.Database.Repositories.RBranch;
 import org.rmj.g3appdriver.GRider.Database.Repositories.RBranchLoanApplication;
 import org.rmj.g3appdriver.GRider.Database.Repositories.RCountry;
 import org.rmj.g3appdriver.GRider.Database.Repositories.RCreditApplicant;
@@ -59,10 +60,10 @@ public class VMReviewLoanApp extends AndroidViewModel {
     private final RImageInfo poImage;
     private EImageInfo poPhoto;
     private String TransNox;
-
     private ECreditApplicantInfo poInfo = new ECreditApplicantInfo();
-
+    private GOCASApplication poGOCas;
     private final MutableLiveData<List<ReviewAppDetail>> plAppDetail = new MutableLiveData<>();
+    private final MutableLiveData<String> brnName = new MutableLiveData<>();
 
     public interface OnFetchReviewListener{
         void OnDetailLoad(List<ReviewAppDetail> detailList);
@@ -75,6 +76,7 @@ public class VMReviewLoanApp extends AndroidViewModel {
         this.plAppDetail.setValue(new ArrayList<>());
         this.poLoan = new RBranchLoanApplication(application);
         this.poImage = new RImageInfo(application);
+        this.poGOCas = GOCASHolder.getInstance().getGOCAS();
     }
 
     public void setTransNox(String transNox){
@@ -88,12 +90,14 @@ public class VMReviewLoanApp extends AndroidViewModel {
 
     public void setCreditAppInfo(ECreditApplicantInfo foInfo){
         this.poInfo = foInfo;
+
         new FetchReviewDetail(instance, plAppDetail::setValue).execute(poInfo);
     }
 
     public LiveData<List<ReviewAppDetail>> getAppDetail(){
         return plAppDetail;
     }
+
 
     public void saveImageFile(EImageInfo foImage){
         this.poPhoto = foImage;
@@ -138,7 +142,7 @@ public class VMReviewLoanApp extends AndroidViewModel {
         private final RCountry poCountry;
         private final ROccupation poJobx;
         private final OnFetchReviewListener mListener;
-
+        private RBranch poBranch;
         public FetchReviewDetail(Application application, OnFetchReviewListener listener) {
             this.instance = application;
             this.poModel = new RMcModel(instance);
@@ -146,18 +150,20 @@ public class VMReviewLoanApp extends AndroidViewModel {
             this.poTown = new RTown(instance);
             this.poCountry = new RCountry(instance);
             this.poJobx = new ROccupation(instance);
+            this.poBranch = new RBranch(instance);
             this.mListener = listener;
         }
 
         @Override
         protected List<ReviewAppDetail> doInBackground(ECreditApplicantInfo... eCreditApplicantInfos) {
             ECreditApplicantInfo poInfo = eCreditApplicantInfos[0];
+
             List<ReviewAppDetail> loListDetl = new ArrayList<>();
             try {
                 GOCASApplication loGOCas = new GOCASApplication();
                 loGOCas.setData(new GoCasBuilder(poInfo).getConstructedDetailedInfo());
                 loListDetl.add(new ReviewAppDetail(true, "Purchase Info", "", ""));
-                loListDetl.add(new ReviewAppDetail(false, "", "Branch", loGOCas.PurchaseInfo().getPreferedBranch()));
+                loListDetl.add(new ReviewAppDetail(false, "", "Branch", poBranch.getBranchNameForNotification(loGOCas.PurchaseInfo().getPreferedBranch())));
 
                 loListDetl.add(new ReviewAppDetail(false, "", "Unit Applied", loGOCas.PurchaseInfo().getBrandName()));
 
@@ -625,10 +631,8 @@ public class VMReviewLoanApp extends AndroidViewModel {
                     loListDetl.add(new ReviewAppDetail(false, "", "Reference " + (i+1), ""));
                     loListDetl.add(new ReviewAppDetail(false, "", "Fullname ", loExp.get("sRefrNmex").toString()));
                     loListDetl.add(new ReviewAppDetail(false, "", "Mobile No. ", loExp.get("sRefrMPNx").toString()));
-                    DTownInfo.TownProvinceName rfBPlace = poTown.getTownProvinceName(loExp.get("sRefrTown").toString());
-                    String rfBirthPlace = rfBPlace.sTownName + ", " + rfBPlace.sProvName;
                     loListDetl.add(new ReviewAppDetail(false, "", "Address ", loExp.get("sRefrAddx").toString()));
-                    loListDetl.add(new ReviewAppDetail(false, "", "Town ", rfBirthPlace));
+                    loListDetl.add(new ReviewAppDetail(false, "", "Town ", loExp.get("sRefrTown").toString()));
                 }
 
 //                CO-MAKER
