da1dd0ab0df856788667fa4dc56b7981303eacbf
mikegarcia8748 <76087753+mikegarcia8748@users.noreply.github.com>
2022-02-22 14:53:46 +0800

    new Module CreditEvaluation

---
+++ g3appdriver/src/main/java/org/rmj/g3appdriver/GRider/Database/DataAccessObject/DCreditOnlineApplicationCI.java
@@ -0,0 +1,21 @@
+package org.rmj.g3appdriver.GRider.Database.DataAccessObject;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Query;
+
+import org.rmj.g3appdriver.GRider.Database.Entities.ECreditOnlineApplicationCI;
+
+import java.util.List;
+
+@Dao
+public interface DCreditOnlineApplicationCI {
+
+    @Query("SELECT * FROM Credit_Online_Application_CI WHERE cTranStat = '0'")
+    LiveData<List<ECreditOnlineApplicationCI>> getForEvaluationList();
+
+    @Query("Update Credit_Online_Application_CI SET cTranStat = '1'")
+    void UpdateTransaction();
+
+
+}
