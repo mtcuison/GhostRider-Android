5f1ffe81feb5210c46a8b214d6ec41a70af74436
Kent <dkentpatrickd@gmail.com>
2022-02-24 09:59:48 +0800

    Migrate Branch Performance Line chart from Activity to Fragment.

--- ahMonitoring/src/main/java/org/rmj/guanzongroup/ghostrider/ahmonitoring/Activity/Activity_BranchPerformance.java
+++ ahMonitoring/src/main/java/org/rmj/guanzongroup/ghostrider/ahmonitoring/Activity/Activity_BranchPerformance.java
@@ -11,201 +11,96 @@
 
 package org.rmj.guanzongroup.ghostrider.ahmonitoring.Activity;
 
-import static org.rmj.g3appdriver.GRider.Constants.AppConstants.CHART_MONTH_LABEL;
-
-import android.content.res.ColorStateList;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.DefaultItemAnimator;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
-import com.github.mikephil.charting.highlight.Highlight;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
-import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
-
-import org.rmj.g3appdriver.GRider.Etc.BranchPerformancePeriod;
-import org.rmj.guanzongroup.ghostrider.ahmonitoring.Adapter.BranchPerformanceAdapter;
+import org.rmj.guanzongroup.ghostrider.ahmonitoring.Etc.FragmentAdapter;
+import org.rmj.guanzongroup.ghostrider.ahmonitoring.Etc.NonSwipeableViewPager;
+import org.rmj.guanzongroup.ghostrider.ahmonitoring.Fragment.Fragment_BranchPerformance_BarChart;
+import org.rmj.guanzongroup.ghostrider.ahmonitoring.Fragment.Fragment_BranchPerformance_LineChart;
+import org.rmj.guanzongroup.ghostrider.ahmonitoring.Fragment.Fragment_BranchPerformance_PieChart;
 import org.rmj.guanzongroup.ghostrider.ahmonitoring.R;
 import org.rmj.guanzongroup.ghostrider.ahmonitoring.ViewModel.VMBranchMonitor;
 
-import java.util.ArrayList;
 import java.util.Objects;
 
-public class Activity_BranchPerformance extends AppCompatActivity implements OnChartValueSelectedListener {
+public class Activity_BranchPerformance extends AppCompatActivity {
+    private static Activity_BranchPerformance instance;
     private VMBranchMonitor mViewModel;
-    private RecyclerView recyclerView;
-    private BranchPerformanceAdapter adapter;
-    private String[] brnSales = {"MC Sales","SP Sales","JO Sales"};
-    private String brnCD;
-    ArrayList<Entry> chartValues;
-    private LineChart lineChart;
-    public int width;
-    public int height;
-    private TextView lblBranch;
-
-    private ColorStateList poColor;
-    private TextView lblItem1;
-    private TextView lblItem2;
-    private TextView lblSelectd;
+    private Toolbar toolbar;
+    private NonSwipeableViewPager viewPager;
+    private static String sBranchCd = "";
+    private Fragment[] poPages = new Fragment[] {
+            new Fragment_BranchPerformance_LineChart(),
+            new Fragment_BranchPerformance_PieChart(),
+            new Fragment_BranchPerformance_BarChart()
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_branch_performance);
-        DisplayMetrics metrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(metrics);
-        width = metrics.widthPixels;
-        height = metrics.heightPixels;
+        instance = this;
+        sBranchCd = getIntent().getStringExtra("brnCD");
         mViewModel = new ViewModelProvider(Activity_BranchPerformance.this).get(VMBranchMonitor.class);
-        brnCD = getIntent().getStringExtra("brnCD");
-        mViewModel.getBranchName(brnCD).observe(Activity_BranchPerformance.this,bnName->{
-            lblBranch.setText(bnName);
-        });
         initWidgets();
-
-        mViewModel.getType().observe(this, s -> setChartValue(s));
-    }
-    private void initWidgets(){
-        lblBranch = findViewById(R.id.tvBranch);
-        recyclerView = findViewById(R.id.recyclerview_branch_performance);
-        lineChart = findViewById(R.id.activity_branch_linechart);
-        Toolbar toolbar = findViewById(R.id.toolbar_branch_performance);
-        setSupportActionBar(toolbar);
-        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
-
-        lblItem1 = findViewById(R.id.item1);
-        lblItem2 = findViewById(R.id.item2);
-        lblSelectd = findViewById(R.id.select);
-        poColor = lblItem2.getTextColors();
-        lblItem1.setOnClickListener(new TabClickHandler());
-        lblItem2.setOnClickListener(new TabClickHandler());
-    }
-    public void setChartValue(String sales){
-        mViewModel.getAllBranchPerformanceInfoByBranch(brnCD).observe(Activity_BranchPerformance.this, eperformance ->{
-            chartValues = new ArrayList<>();
-            if (sales.equalsIgnoreCase("MC")){
-                for (int x = 0; x< eperformance.size(); x++){
-                    chartValues.add(new Entry(x, eperformance.get(x).getMCActual()));
-                }
-
-                BranchPerformanceAdapter.setIndexPosition(-1);
-            }else {
-                for (int x = 0; x< eperformance.size(); x++){
-                    chartValues.add(new Entry(x, eperformance.get(x).getSPActual()));
-                }
-
-                BranchPerformanceAdapter.setIndexPosition(-1);
-            }
-            LineDataSet lineDataSet1 = new LineDataSet(chartValues, "");
-
-            // Set line attributes
-            lineDataSet1.setLineWidth(2);
-            lineDataSet1.setColors(getResources().getColor(R.color.guanzon_orange));
-            //ArrayList<ILineDataSet> Contains list of LineDataSets
-            ArrayList<ILineDataSet> dataSets = new ArrayList<>();
-            dataSets.add(lineDataSet1);
-
-
-            // LineData Contains ArrayList<ILineDataSet>
-            LineData data = new LineData(dataSets);
-            lineChart.setData(data);
-            lineChart.setDescription(null);
-            lineChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(BranchPerformancePeriod.getBranchTableLabel(eperformance)));
-            lineChart.setDoubleTapToZoomEnabled(false);
-            lineChart.getXAxis().setTextSize(14f);
-            lineChart.setExtraOffsets(0,0,10f,18f);
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(width, (height/3));
-            lineChart.setLayoutParams(params);
-            lineChart.getLegend().setEnabled(false);
-            lineChart.setDrawGridBackground(false);
-            lineChart.setDrawBorders(true);
-            lineChart.setBorderWidth(1);
-            lineChart.setBorderColor(getResources().getColor(R.color.color_dadada));
-            lineChart.animateX(500);
-            XAxis xAxis = lineChart.getXAxis();
-            xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-            lineChart.setOnChartValueSelectedListener(this);
-            lineChart.invalidate();
-//          SET RECYLERVIEW
-            adapter = new BranchPerformanceAdapter(this,sales, eperformance);
-            recyclerView.setHasFixedSize(true);
-            recyclerView.setItemAnimator(new DefaultItemAnimator());
-            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this,  LinearLayoutManager.VERTICAL, false);
-            linearLayoutManager.setReverseLayout(true);
-            linearLayoutManager.setStackFromEnd(true);
-            recyclerView.setLayoutManager(linearLayoutManager);
-
-            recyclerView.setAdapter(adapter);
-
-        });
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
-    public void onValueSelected(Entry e, Highlight h) {
-        try{
-            BranchPerformanceAdapter.setIndexPosition((int) e.getX());
-            adapter.notifyDataSetChanged();
-        }catch (NullPointerException ex) {
-            ex.printStackTrace();
-        }
-
+        setUpToolbar();
     }
 
     @Override
-    public void onNothingSelected() {
-
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.chart_menu, menu);
+        return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if(item.getItemId() == android.R.id.home){
             finish();
+        } else if(item.getItemId() == R.id.line_chart) {
+            moveToPageNumber(0);
+        } else if(item.getItemId() == R.id.pie_chart) {
+            moveToPageNumber(1);
+        } else if(item.getItemId() == R.id.bar_chart) {
+            moveToPageNumber(2);
         }
         return super.onOptionsItemSelected(item);
     }
 
     @Override
-    public void finish() {
-        super.finish();
-        overridePendingTransition(R.anim.anim_intent_slide_in_left, R.anim.anim_intent_slide_out_right);
+    public void onBackPressed() {
+        finish();
     }
 
-
-    private class TabClickHandler implements View.OnClickListener{
-
-        @Override
-        public void onClick(View view) {
-            if (view.getId() == R.id.item1){
-                lblSelectd.animate().x(0).setDuration(100);
-                lblItem1.setTextColor(Color.WHITE);
-                lblItem2.setTextColor(poColor);
-                mViewModel.setType("MC");
-            } else if (view.getId() == R.id.item2){
-                lblItem1.setTextColor(poColor);
-                lblItem2.setTextColor(Color.WHITE);
-                int size = lblItem2.getWidth();
-                lblSelectd.animate().x(size).setDuration(100);
-                mViewModel.setType("SP");
-            }
-        }
+    public static Activity_BranchPerformance getInstance() {
+        return instance;
     }
+
+    public String getBranchCode() {
+        return sBranchCd;
+    }
+
+    private void initWidgets(){
+        toolbar = findViewById(R.id.toolbar);
+        viewPager = findViewById(R.id.viewpager);
+        viewPager.setAdapter(new FragmentAdapter(getSupportFragmentManager(), poPages));
+    }
+
+    private void setUpToolbar() {
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Branch Performance Monitoring");
+    }
+
+    private void moveToPageNumber(int fnPageNum){
+        viewPager.setCurrentItem(fnPageNum);
+    }
+
 }
---
+++ ahMonitoring/src/main/java/org/rmj/guanzongroup/ghostrider/ahmonitoring/Fragment/Fragment_BranchPerformance_BarChart.java
@@ -0,0 +1,46 @@
+/*
+ * Created by Android Team MIS-SEG Year 2021
+ * Copyright (c) 2021. Guanzon Central Office
+ * Guanzon Bldg., Perez Blvd., Dagupan City, Pangasinan 2400
+ * Project name : GhostRider_Android
+ * Module : GhostRider_Android.ahMonitoring
+ * Electronic Personnel Access Control Security System
+ * project file created : 2/24/22, 9:06 AM
+ * project file last modified : 2/24/22, 9:06 AM
+ */
+
+package org.rmj.guanzongroup.ghostrider.ahmonitoring.Fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.rmj.guanzongroup.ghostrider.ahmonitoring.R;
+
+public class Fragment_BranchPerformance_BarChart extends Fragment {
+
+    public Fragment_BranchPerformance_BarChart() { }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_branch_performance_bar_chart,
+                container, false);
+        initViews(view);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    private void initViews(View v) {
+
+    }
+}
