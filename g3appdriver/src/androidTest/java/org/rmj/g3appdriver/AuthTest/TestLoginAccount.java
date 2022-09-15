4fe440c661f1992d4371eb9586b4a40897735bc4
mikegarcia8748 <76087753+mikegarcia8748@users.noreply.github.com>
2022-09-15 17:22:02 +0800

    Adjusting Employee leave, business trip and user login to TDD for better testing capability on future upgrades

--- g3appdriver/src/androidTest/java/org/rmj/g3appdriver/AuthTest/TestLoginAccount.java
+++ g3appdriver/src/androidTest/java/org/rmj/g3appdriver/AuthTest/TestLoginAccount.java
@@ -4,6 +4,7 @@ package org.rmj.g3appdriver.AuthTest;
 import static org.junit.Assert.assertTrue;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.runner.AndroidJUnit4;
@@ -14,19 +15,25 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.rmj.g3appdriver.GRider.Database.Repositories.REmployee;
+import org.rmj.g3appdriver.etc.AppConfigPreference;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(AndroidJUnit4.class)
 public class TestLoginAccount {
+    private static final String TAG = TestLoginAccount.class.getSimpleName();
 
     private Application instance;
 
     private REmployee poUser;
 
+    private boolean isSuccess = false;
+    private String message;
+
     @Before
     public void setup() throws Exception{
         instance = ApplicationProvider.getApplicationContext();
         poUser = new REmployee(instance);
+        AppConfigPreference.getInstance(instance).setTestCase(true);
     }
 
     @Test
@@ -39,5 +46,28 @@ public class TestLoginAccount {
     public void test02LoginAccount() throws Exception{
         REmployee.UserAuthInfo loAuth = new REmployee.UserAuthInfo("mikegarcia8748@gmail.com", "123456", "09171870011");
         assertTrue(poUser.AuthenticateUser(loAuth));
+    }
+
+    @Test
+    public void test03GetAuthorizeFeatures() throws Exception{
+        assertTrue(poUser.GetUserAuthorizeAccess());
+    }
+
+    @Test
+    public void test04TestLoginWithValidation() throws Exception{
+        REmployee.UserAuthInfo loAuth = new REmployee.UserAuthInfo("mikegarcia8748@gmail.com", "123456", "09171870011");
+        if(!loAuth.isAuthInfoValid()){
+            message = loAuth.getMessage();
+            isSuccess = false;
+        } else {
+            if (!poUser.AuthenticateUser(loAuth)) {
+                message = poUser.getMessage();
+                return;
+            }
+        }
+
+
+        Log.d(TAG, message);
+        assertTrue(isSuccess);
     }
 }
