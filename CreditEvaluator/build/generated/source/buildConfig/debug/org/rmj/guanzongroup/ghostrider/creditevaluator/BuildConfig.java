207a5966202d3ce8b32c2137056db2a8a1f8e1e5
mikegarcia8748 <mikegarcia8748@gmail.com>
2022-09-19 18:43:59 +0800

    Fix the OB, Leave Application and approval.

---
+++ CreditEvaluator/build/generated/source/buildConfig/debug/org/rmj/guanzongroup/ghostrider/creditevaluator/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.rmj.guanzongroup.ghostrider.creditevaluator;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "org.rmj.guanzongroup.ghostrider.creditevaluator";
+  public static final String BUILD_TYPE = "debug";
+}
