89db7fa99f960050a92d42add2a5dc0149bca1e6
mikegarcia <mikegarcia8748@gmail.com>
2022-11-30 16:31:35 +0800

    Fixing notification class

--- appdriver/src/main/java/org/rmj/g3appdriver/lib/Notifications/iNotification.java
+++ appdriver/src/main/java/org/rmj/g3appdriver/lib/Notifications/iNotification.java
@@ -1,8 +1,26 @@
 package org.rmj.g3appdriver.lib.Notifications;
 
+import androidx.lifecycle.LiveData;
+
 import com.google.firebase.messaging.RemoteMessage;
 
+import org.rmj.g3appdriver.lib.Notifications.model.NotificationItemList;
+
+import java.util.List;
+
 public interface iNotification {
-    void Save(RemoteMessage foVal);
-    void CreateNotification(String title, String message);
+    boolean Save(RemoteMessage foVal);
+
+    /**
+     *
+     * @param val parameters refer to what type of response must be send
+     *            Receive, Read
+     */
+    boolean SendResponse(String val);
+
+    boolean CreateNotification(String title, String message);
+
+    LiveData<List<NotificationItemList>> GetNotificationList();
+
+    String getMessage();
 }
