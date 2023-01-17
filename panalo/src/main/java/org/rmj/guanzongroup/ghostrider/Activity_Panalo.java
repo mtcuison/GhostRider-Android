bdf30aceb54a6037fdbaa60586d5067587ac2c62
DMRosario+ <markztar143@gmail.com>
2023-01-17 17:28:14 +0800

    create activity_panalo.xml

---
+++ panalo/src/main/java/org/rmj/guanzongroup/ghostrider/Activity_Panalo.java
@@ -0,0 +1,14 @@
+package org.rmj.guanzongroup.ghostrider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Panalo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_panalo);
+    }
+}
