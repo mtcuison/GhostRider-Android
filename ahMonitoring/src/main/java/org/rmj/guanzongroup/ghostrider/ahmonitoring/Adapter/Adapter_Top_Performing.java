daa7f6c04a7ec42bdf1e9d897ae0074f0396cabd
Jeitee <iamjeitee.8@gmail.com>
2023-03-03 16:15:22 +0800

    03/03/2023
    * create ui for area monitoring
    * list of top performing branch ui

---
+++ ahMonitoring/src/main/java/org/rmj/guanzongroup/ghostrider/ahmonitoring/Adapter/Adapter_Top_Performing.java
@@ -0,0 +1,16 @@
+package org.rmj.guanzongroup.ghostrider.ahmonitoring.Adapter;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.rmj.guanzongroup.ghostrider.ahmonitoring.R;
+
+public class Adapter_Top_Performing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_top_performing);
+    }
+}
