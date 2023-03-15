3f96bd583faa4b1d3be3ee671613dca1d4de6221
Jeitee <iamjeitee.8@gmail.com>
2023-03-15 16:12:36 +0800

    03/15/2023
    display of line graph for mc sp and jo for branch manager

--- ahMonitoring/src/main/java/org/rmj/guanzongroup/ghostrider/ahmonitoring/Fragment/Fragment_BranchMonitor.java
+++ ahMonitoring/src/main/java/org/rmj/guanzongroup/ghostrider/ahmonitoring/Fragment/Fragment_BranchMonitor.java
@@ -22,6 +22,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.github.mikephil.charting.charts.LineChart;
@@ -31,10 +32,12 @@ import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.progressindicator.CircularProgressIndicator;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.textview.MaterialTextView;
 
-import org.rmj.g3appdriver.dev.Database.Entities.EBranchPerformance;
+import org.rmj.g3appdriver.dev.Database.DataAccessObject.DBranchPerformance;
 import org.rmj.guanzongroup.ghostrider.ahmonitoring.R;
 import org.rmj.guanzongroup.ghostrider.ahmonitoring.ViewModel.VMBranchMonitor;
 
@@ -49,35 +52,52 @@ public class Fragment_BranchMonitor extends Fragment {
     private TabLayout tabLayout;
     private String BranchCd;
     private MaterialTextView sample;
-
+    private MaterialCardView cvMCSales, cvSPSales, cvJobOrder;
     public static Fragment_BranchMonitor newInstance() {
         return new Fragment_BranchMonitor();
     }
-
+    private CircularProgressIndicator mcIndicator,spIndicator,joIndicator;
+    private MaterialTextView mcGoalPerc,spGoalPerc,joGoalPerc,mcFraction,spFraction,joFraction;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         mViewModel = new ViewModelProvider(this).get(VMBranchMonitor.class);
-        View view = inflater.inflate(R.layout.fragment_branch_monitor, container, false);
 
-        tabLayout = view.findViewById(R.id.tabLayout);
-        lineChart = view.findViewById(R.id.activity_branch_linechart);
-        tabLayout.addTab(tabLayout.newTab().setText("MC Sales"));
-        tabLayout.addTab(tabLayout.newTab().setText("SP Sales"));
-        tabLayout.addTab(tabLayout.newTab().setText("Joborder"));
-//        initWidgets(view);
         mViewModel.getEmployeeInfo().observe(getViewLifecycleOwner(), eEmployeeInfo -> {
             try {
-                //lblEmail.setText(eEmployeeInfo.getEmailAdd());
-//                lblUserLvl.setText(DeptCode.parseUserLevel(eEmployeeInfo.getEmpLevID()));
                 BranchCd = eEmployeeInfo.getBranchCD();
 
             } catch (Exception e){
                 e.printStackTrace();
             }
         });
+        View view = inflater.inflate(R.layout.fragment_branch_monitor, container, false);
+        mcIndicator = view.findViewById(R.id.cpi_mc_sales);
+        spIndicator = view.findViewById(R.id.cpi_sp_sales);
+        joIndicator = view.findViewById(R.id.cpi_jo);
 
-        mViewModel.getAllBranchPerformanceInfoByBranch(BranchCd).observe(getViewLifecycleOwner(),  BranchPerforamancebyMC -> {
+        mcGoalPerc = view.findViewById(R.id.lbl_mc_percentage);
+        spGoalPerc = view.findViewById(R.id.lbl_sp_percentage);
+        joGoalPerc = view.findViewById(R.id.lbl_jo_percentage);
+
+        mcFraction = view.findViewById(R.id.lbl_mc_goal);
+        spFraction = view.findViewById(R.id.lbl_sp_goal);
+        joFraction = view.findViewById(R.id.lbl_jo_goal);
+
+        cvMCSales = view.findViewById(R.id.cv_mc_sales);
+        cvSPSales = view.findViewById(R.id.cv_sp_sales);
+        cvJobOrder = view.findViewById(R.id.cv_jo);
+
+        tabLayout = view.findViewById(R.id.tabLayout);
+        lineChart = view.findViewById(R.id.activity_branch_linechart);
+
+        tabLayout.addTab(tabLayout.newTab().setText("MC Sales"));
+        tabLayout.addTab(tabLayout.newTab().setText("SP Sales"));
+        tabLayout.addTab(tabLayout.newTab().setText("Joborder"));
+
+        initGoalPercentage();
+
+        mViewModel.GetMCSalesPeriodicPerformance(BranchCd).observe(getViewLifecycleOwner(),  BranchPerforamancebyMC -> {
             try{
                 InitializeBranchList(BranchPerforamancebyMC, 0);
             }catch (Exception e){
@@ -90,7 +110,7 @@ public class Fragment_BranchMonitor extends Fragment {
             public void onTabSelected(TabLayout.Tab tab) {
                 if(tab.getPosition() == 0){
 
-                    mViewModel.getAllBranchPerformanceInfoByBranch(BranchCd).observe(getViewLifecycleOwner(),  BranchPerforamancebyMC -> {
+                    mViewModel.GetMCSalesPeriodicPerformance(BranchCd).observe(getViewLifecycleOwner(),  BranchPerforamancebyMC -> {
                         try{
                             InitializeBranchList(BranchPerforamancebyMC, tab.getPosition());
 
@@ -99,7 +119,7 @@ public class Fragment_BranchMonitor extends Fragment {
                         }
                     });
                 } else if (tab.getPosition() == 1) {
-                   mViewModel.getAllBranchPerformanceInfoByBranch(BranchCd).observe(getViewLifecycleOwner(),  BranchPerforamancebySP -> {
+                   mViewModel.GetSPSalesPeriodicPerformance(BranchCd).observe(getViewLifecycleOwner(),  BranchPerforamancebySP -> {
                         try{
                             InitializeBranchList(BranchPerforamancebySP, tab.getPosition());
                         }catch (Exception e){
@@ -107,9 +127,9 @@ public class Fragment_BranchMonitor extends Fragment {
                         }
                     });
                 } else{
-                    mViewModel.getAllBranchPerformanceInfoByBranch(BranchCd).observe(getViewLifecycleOwner(),  BranchPerforamancebySP -> {
+                    mViewModel.GetJobOrderPeriodicPerformance(BranchCd).observe(getViewLifecycleOwner(),  BranchPerforamancebyJO -> {
                         try{
-                            InitializeBranchList(BranchPerforamancebySP, tab.getPosition());
+                            InitializeBranchList(BranchPerforamancebyJO, tab.getPosition());
                         }catch (Exception e){
                             e.printStackTrace();
                         }
@@ -128,7 +148,7 @@ public class Fragment_BranchMonitor extends Fragment {
         return view;
     }
 
-    private void InitializeBranchList(List<EBranchPerformance> list, int priority){
+    private void InitializeBranchList(List<DBranchPerformance.PeriodicalPerformance> list, int priority){
 
 //            AreaMonitoringAdapter loAdapter = new AreaMonitoringAdapter(list, priority, new AreaMonitoringAdapter.OnBranchPerformanceClickListener() {
 //                @Override
@@ -152,10 +172,10 @@ public class Fragment_BranchMonitor extends Fragment {
             values2 = new ArrayList<>();
             values3 = new ArrayList<>();
             for (int x = 0; x< list.size(); x++){
-                EBranchPerformance info = list.get(x);
-                values1.add(new Entry(info.getMCGoalxx(), info.getMCActual()));
-                values2.add(new Entry(x, info.getSPGoalxx()));
-                values3.add(new Entry(x, info.getJOGoalxx()));
+                DBranchPerformance.PeriodicalPerformance info = list.get(x);
+                values1.add(new Entry(Float.valueOf(info.Goal), Float.valueOf(info.Actual),Float.valueOf(info.Period)));
+                values2.add(new Entry(Float.valueOf(info.Goal), Float.valueOf(info.Actual),Float.valueOf(info.Period)));
+                values3.add(new Entry(Float.valueOf(info.Goal), Float.valueOf(info.Actual),Float.valueOf(info.Period)));
 
 //                Log.e("MC", String.valueOf(info.getMCActual()));
 //                Log.e("Val1", info.getBranchCd() + " " +  info.getMCGoalxx());
@@ -194,7 +214,6 @@ public class Fragment_BranchMonitor extends Fragment {
             lineChart.setDescription(desc);
             lineChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(CHART_MONTH_LABEL));
             lineChart.setDoubleTapToZoomEnabled(false);
-
     }
 
     @Override
@@ -244,7 +263,56 @@ public class Fragment_BranchMonitor extends Fragment {
 //        });
         // TODO: Use the ViewModel
     }
-
+    public void initGoalPercentage(){
+        mViewModel.GetCurrentMCSalesPerformance(BranchCd).observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String mc_goal) {
+                try {
+                    mcFraction.setText(mc_goal);
+                    if (mc_goal.contains("/")){
+                        String[] rat = mc_goal.split("/");
+                        double ratio =Double.parseDouble(rat[0]) / Double.parseDouble(rat[1]) * 100;
+                        mcGoalPerc.setText(String.valueOf(Math.round(ratio)) + "%");
+                        mcIndicator.setProgress((int) (Math.round(ratio)));
+                    }
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        mViewModel.GetCurrentSPSalesPerformance(BranchCd).observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String sp_goal) {
+                try {
+                    spFraction.setText(sp_goal);
+                    if (sp_goal.contains("/")){
+                        String[] rat = sp_goal.split("/");
+                        double ratio =Double.parseDouble(rat[0]) / Double.parseDouble(rat[1]) * 100;
+                        spGoalPerc.setText(String.valueOf(Math.round(ratio)) + "%");
+                        spIndicator.setProgress((int) (Math.round(ratio)));
+                    }
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        mViewModel.GetJobOrderPerformance(BranchCd).observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String jo_goal) {
+                try {
+                    joFraction.setText(jo_goal);
+                    if (jo_goal.contains("/")){
+                        String[] rat = jo_goal.split("/");
+                        double ratio =Double.parseDouble(rat[0]) / Double.parseDouble(rat[1]) * 100;
+                        joGoalPerc.setText(String.valueOf(Math.round(ratio)) + "%");
+                        joIndicator.setProgress((int) (Math.round(ratio)));
+                    }
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
 
 }
