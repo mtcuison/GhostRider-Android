f5746244d8a933195c9f08d46909e720b7b0f248
mikegarcia <mikegarcia8748@gmail.com>
2023-03-16 13:31:22 +0800

    Added notification badge for panalo raffle draw

--- panalo/src/main/java/org/rmj/guanzongroup/ghostrider/Fragment/Fragment_PanaloContainer.java
+++ panalo/src/main/java/org/rmj/guanzongroup/ghostrider/Fragment/Fragment_PanaloContainer.java
@@ -2,16 +2,26 @@ package org.rmj.guanzongroup.ghostrider.Fragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.experimental.UseExperimental;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.badge.BadgeUtils;
+import com.google.android.material.badge.ExperimentalBadgeUtils;
 import com.google.android.material.tabs.TabLayout;
 
+import org.rmj.g3appdriver.dev.Database.Entities.ERaffleStatus;
 import org.rmj.guanzongroup.ghostrider.Adapter.FragmentAdapter;
 import org.rmj.guanzongroup.ghostrider.R;
+import org.rmj.guanzongroup.ghostrider.ViewModel.VMPanaloContainer;
 
 import java.util.Objects;
 
@@ -21,6 +31,8 @@ import java.util.Objects;
  * create an instance of this fragment.
  */
 public class Fragment_PanaloContainer extends Fragment {
+
+    private VMPanaloContainer mViewModel;
     private View view;
     private TabLayout tabLayout;
     private ViewPager viewPager;
@@ -72,6 +84,7 @@ public class Fragment_PanaloContainer extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        mViewModel = new ViewModelProvider(requireActivity()).get(VMPanaloContainer.class);
         view= inflater.inflate(R.layout.fragment_panalo_container, container, false);
         fragment = new Fragment[]{new Fragment_Raffle(), new Fragment_Rewards()};
         tabLayout = view.findViewById(R.id.tabLayout);
@@ -87,6 +100,18 @@ public class Fragment_PanaloContainer extends Fragment {
             Objects.requireNonNull(tabLayout.getTabAt(0)).setText("Raffle Draw");
             Objects.requireNonNull(tabLayout.getTabAt(1)).setText("Redemption");
         }
+
+        mViewModel.GetRaffleStatus().observe(requireActivity(), new Observer<ERaffleStatus>() {
+            @Override
+            public void onChanged(ERaffleStatus status) {
+                try{
+                    InitializeRaffleBadge(status.getHasRffle());
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
 
             @Override
@@ -105,5 +130,22 @@ public class Fragment_PanaloContainer extends Fragment {
             }
         });
         return view;
+    }
+
+    @UseExperimental(markerClass = ExperimentalBadgeUtils.class)
+    private void InitializeRaffleBadge(int status){
+        TabLayout.Tab loTab = tabLayout.getTabAt(0);
+        BadgeDrawable loBadge = loTab.getOrCreateBadge();
+        View loMenuView = tabLayout.getTabAt(0).getCustomView();
+        switch (status){
+            case 0:
+                BadgeUtils.attachBadgeDrawable(loBadge, loMenuView);
+            case 1:
+                BadgeUtils.attachBadgeDrawable(loBadge, loMenuView);
+            case 2:
+                BadgeUtils.attachBadgeDrawable(loBadge, loMenuView);
+            default:
+                loTab.removeBadge();
+        }
     }
 }
