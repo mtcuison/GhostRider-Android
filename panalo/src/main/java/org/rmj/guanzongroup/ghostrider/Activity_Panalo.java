ac1488264f148fbe4bf5366745d77c539db7effd
mnv121 <valenciamaynardn@gmail.com>
2023-05-22 15:01:38 +0800

    activity_brand_selection.xml activity_brand_selection.xml design

--- panalo/src/main/java/org/rmj/guanzongroup/ghostrider/Activity_Panalo.java
+++ panalo/src/main/java/org/rmj/guanzongroup/ghostrider/Activity_Panalo.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Activity_Panalo extends AppCompatActivity {
+public class  Activity_Panalo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
