023bb2ba9a4bca6fdc8c2e510052185e1abfd7ed
ggutoman <ggutoman@gmail.com>
2023-07-19 17:27:32 -0600

    created testing class for pacita reward

--- pacitaReward/src/androidTest/java/org/rmj/guanzongroup/pacitareward/ExampleInstrumentedTest.java
+++ pacitaReward/src/androidTest/java/org/rmj/guanzongroup/pacitareward/ExampleInstrumentedTest.java
@@ -1,12 +1,29 @@
 package org.rmj.guanzongroup.pacitareward;
 
-import android.content.Context;
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
+import android.content.Context;
+import android.view.View;
+
+import androidx.fragment.app.testing.FragmentScenario;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.hamcrest.Matcher;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.rmj.guanzongroup.pacitareward.Activity.Activity_BranchList;
+import org.rmj.guanzongroup.pacitareward.Adapter.RecyclerViewAdapter_BranchList;
+import org.rmj.guanzongroup.pacitareward.Dialog.Dialog_SelectAction;
+import org.rmj.guanzongroup.pacitareward.Fragments.Fragment_BranchList;
 
 import static org.junit.Assert.*;
 
@@ -21,6 +38,19 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        //validate if package is under this test
         assertEquals("org.rmj.guanzongroup.pacitareward.test", appContext.getPackageName());
+    }
+
+    @Rule
+    public ActivityScenarioRule<Activity_BranchList> activityRule =
+            new ActivityScenarioRule<>(Activity_BranchList.class); //launch primary activity before test
+    @Rule
+    public FragmentScenario fragmentScenario =
+            FragmentScenario.launchInContainer(Fragment_BranchList.class);
+
+    @Test
+    public void openBranchRate(){
+        onData(withId(R.id.branch_list)).inAdapterView(withId(R.id.item_branch)).perform(click());
     }
 }
--- pacitaReward/src/test/java/org/rmj/guanzongroup/pacitareward/ExampleUnitTest.java
+++ pacitaReward/src/test/java/org/rmj/guanzongroup/pacitareward/ExampleUnitTest.java
@@ -1,17 +1,15 @@
 package org.rmj.guanzongroup.pacitareward;
 
-import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
-import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@RunWith(JUnit4.class)
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
+
 }
