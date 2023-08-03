71d0a8ca9d540caa5a9372c6137442a0d952582b
mikegarcia <mikegarcia8748@gmail.com>
2023-08-03 13:39:25 +0800

    Revise the design of activity_login.xml.
    changed the local address on ConnectionUtil and WebApi for local testing

--- appdriver/src/main/java/org/rmj/g3appdriver/utils/ConnectionUtil.java
+++ appdriver/src/main/java/org/rmj/g3appdriver/utils/ConnectionUtil.java
@@ -43,7 +43,7 @@ public class ConnectionUtil {
     private final Context context;
     private String message;
 
-    private static final String LOCAL = "http://192.168.10.27";
+    private static final String LOCAL = "http://192.168.11.29";
     private static final String PRIMARY_LIVE = "https://restgk.guanzongroup.com.ph";
 //    private static final String SECONDARY_LIVE = "restgk1.guanzongroup.com.ph";
 
