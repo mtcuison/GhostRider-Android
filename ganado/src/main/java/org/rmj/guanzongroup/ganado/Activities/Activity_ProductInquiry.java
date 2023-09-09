896ed13b1d8a73ee0fe8b29dadb9ec26f5f668e8
mikegarcia <mikegarcia8748@gmail.com>
2023-09-09 10:05:02 +0800

    Fixed visibility of UI for cash and installment payments

--- ganado/src/main/java/org/rmj/guanzongroup/ganado/Activities/Activity_ProductInquiry.java
+++ ganado/src/main/java/org/rmj/guanzongroup/ganado/Activities/Activity_ProductInquiry.java
@@ -159,10 +159,10 @@ public class Activity_ProductInquiry extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 mViewModel.getModel().setPaymForm(String.valueOf(position));
                 if(position == 0){
-                    findViewById(R.id.txt_cashPrice).setVisibility(View.VISIBLE);
+                    findViewById(R.id.til_cashPrice).setVisibility(View.VISIBLE);
                     findViewById(R.id.linearInstallment).setVisibility(View.GONE);
                 } else {
-                    findViewById(R.id.txt_cashPrice).setVisibility(View.GONE);
+                    findViewById(R.id.til_cashPrice).setVisibility(View.GONE);
                     findViewById(R.id.linearInstallment).setVisibility(View.VISIBLE);
                 }
             }
