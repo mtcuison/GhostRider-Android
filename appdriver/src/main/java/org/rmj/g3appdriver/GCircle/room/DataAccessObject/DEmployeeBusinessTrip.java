edf3cace82498cee7ecb33b59e015311e790113e
ggutoman <ggutoman@gmail.com>
2024-03-07 15:09:43 +0800

    Fixed bug on displaying leave and ob applications that is less than one month

--- appdriver/src/main/java/org/rmj/g3appdriver/GCircle/room/DataAccessObject/DEmployeeBusinessTrip.java
+++ appdriver/src/main/java/org/rmj/g3appdriver/GCircle/room/DataAccessObject/DEmployeeBusinessTrip.java
@@ -68,8 +68,10 @@ public interface DEmployeeBusinessTrip {
     @Query("UPDATE Employee_Business_Trip SET cSendStat = '1' WHERE sTransNox=:TransNox")
     void updateObApprovalPostedStatus(String TransNox);
 
-    @Query("SELECT * FROM Employee_Business_Trip WHERE sApproved IS NULL AND dApproved IS NULL AND (dDateFrom BETWEEN DATE('now', '-30 day') AND DATE(DATE('now', '-30 day'), \"+30 day\")) " +
-            "OR (dDateThru BETWEEN DATE('now', '-30 day') AND DATE(DATE('now', '-30 day'), \"+30 day\")) ORDER BY dTransact DESC")
+    @Query("SELECT * FROM Employee_Business_Trip " +
+            "WHERE cTranStat = '0' " +
+            "AND (dDateFrom >= DATE('now', '-30 day') AND dDateThru >= DATE('now', '-30 day')) " +
+            "ORDER BY dTransact DESC")
     LiveData<List<EEmployeeBusinessTrip>> getOBListForApproval();
 
     @Query("SELECT * FROM Employee_Business_Trip " +
